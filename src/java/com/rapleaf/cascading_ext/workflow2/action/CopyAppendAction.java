13d44b5770af2fac86b69a9fdfe5411cc287d2cd
bpodgursky <bpodgursky@910646d7-630e-0410-84d6-b3eb9c11e90f>
2012-01-13 19:22:19 +0000

    ANALYSIS-3226
    git-svn-id: https://svn.vpn.rapleaf.com/main/cascading_ext/trunk@99037 910646d7-630e-0410-84d6-b3eb9c11e90f

---
+++ src/java/com/rapleaf/cascading_ext/workflow2/action/CopyAppendAction.java
@@ -0,0 +1,26 @@
+package com.rapleaf.cascading_ext.workflow2.action;
+
+import com.rapleaf.cascading_ext.datastore.BucketDataStore;
+import com.rapleaf.cascading_ext.workflow2.Action;
+
+public class CopyAppendAction extends Action {
+
+  private final BucketDataStore input;
+  private final BucketDataStore target;
+  
+  public CopyAppendAction(String checkpointToken, BucketDataStore input, BucketDataStore target) {
+    super(checkpointToken);
+    
+    this.input = input;
+    this.target = target;
+    
+    readsFrom(input);
+    writesTo(target);
+  }
+
+  @Override
+  protected void execute() throws Exception {
+    this.setStatusMessage("Copy-appending "+input.getPath()+" to "+target.getPath());
+    target.getBucket().copyAppend(input.getBucket());
+  }
+}
---
+++ src/java/com/rapleaf/cascading_ext/workflow2/action/CopyAppendSplitBucketAction.java
@@ -0,0 +1,26 @@
+package com.rapleaf.cascading_ext.workflow2.action;
+
+import com.rapleaf.cascading_ext.datastore.SplitBucketDataStore;
+import com.rapleaf.cascading_ext.workflow2.Action;
+
+public class CopyAppendSplitBucketAction extends Action {
+  
+  private final SplitBucketDataStore input;
+  private final SplitBucketDataStore target;
+  
+  public CopyAppendSplitBucketAction(String checkpoint, SplitBucketDataStore input, SplitBucketDataStore target){
+    super(checkpoint);
+    
+    this.input = input;
+    this.target = target;
+    
+    readsFrom(input);
+    writesTo(target);
+  }
+
+  @Override
+  protected void execute() throws Exception {
+    this.setStatusMessage("Copy-appending "+input.getPath()+" to "+target.getPath());
+    target.getAttributeBucket().copyAppend(input.getAttributeBucket());
+  }
+}
