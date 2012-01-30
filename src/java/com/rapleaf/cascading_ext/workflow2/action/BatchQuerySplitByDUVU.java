83e302f19db3db6ad55993f7a7c803b56956391b
bpodgursky <bpodgursky@910646d7-630e-0410-84d6-b3eb9c11e90f>
2012-01-30 21:54:38 +0000

    add action to batch query split bucket stores
    git-svn-id: https://svn.vpn.rapleaf.com/main/cascading_ext/trunk@100675 910646d7-630e-0410-84d6-b3eb9c11e90f

---
+++ src/java/com/rapleaf/cascading_ext/workflow2/action/BatchQuerySplitByDUVU.java
@@ -0,0 +1,46 @@
+package com.rapleaf.cascading_ext.workflow2.action;
+
+import com.rapleaf.cascading_ext.datastore.BucketDataStore;
+import com.rapleaf.cascading_ext.datastore.SplitBucketDataStore;
+import com.rapleaf.cascading_ext.relevance.Relevance;
+import com.rapleaf.cascading_ext.workflow2.Action;
+import com.rapleaf.types.new_person_data.DataUnitValueUnion;
+
+import java.util.EnumSet;
+import java.util.Set;
+
+public class BatchQuerySplitByDUVU extends Action {
+
+  private final SplitBucketDataStore<DataUnitValueUnion._Fields> splitStore;
+  private final BucketDataStore eids;
+  private final BucketDataStore relevantStore;
+  private final Set<DataUnitValueUnion._Fields> fields;
+
+  private final Relevance.RelevanceFunction relevanceFunction;
+  private final Relevance relevance;
+
+  public BatchQuerySplitByDUVU(String checkpointToken, Relevance relevance, Relevance.RelevanceFunction relevanceFunction,
+                               SplitBucketDataStore splitStore, BucketDataStore eids, BucketDataStore relevantSplitStore) {
+    this(checkpointToken, relevance, relevanceFunction, splitStore, EnumSet.allOf(DataUnitValueUnion._Fields.class),  eids, relevantSplitStore);
+  }
+
+  public BatchQuerySplitByDUVU(String checkpointToken, Relevance relevance, Relevance.RelevanceFunction relevanceFunction, SplitBucketDataStore splitStore, Set<DataUnitValueUnion._Fields> fields, BucketDataStore eids, BucketDataStore relevantSplitStore) {
+    super(checkpointToken);
+
+    this.relevanceFunction = relevanceFunction;
+    this.splitStore = splitStore;
+    this.eids = eids;
+    this.fields = fields;
+    this.relevantStore = relevantSplitStore;
+    this.relevance = relevance;
+
+    readsFrom(eids);
+    readsFrom(splitStore);
+    creates(relevantSplitStore);
+  }
+
+  @Override
+  protected void execute() throws Exception {
+    relevance.setParent(this).batch_query(splitStore.getTap(fields), relevantStore.getTap(), relevanceFunction, eids.getPath(), true, true);
+  }
+}
