05b4e7f573946fa8a094aa01ddee3504a877ca95
andre <andre@910646d7-630e-0410-84d6-b3eb9c11e90f>
2012-04-02 21:10:39 +0000

    allow for the version type to be specified at a later time ANALYSIS-3490
    git-svn-id: https://svn.vpn.rapleaf.com/main/cascading_ext/trunk@107193 910646d7-630e-0410-84d6-b3eb9c11e90f

--- src/java/com/rapleaf/cascading_ext/workflow2/action/HankDomainBuilderAction.java
+++ src/java/com/rapleaf/cascading_ext/workflow2/action/HankDomainBuilderAction.java
@@ -20,13 +20,21 @@ public abstract class HankDomainBuilderAction extends Action {
   protected final Map<Object, Object> properties;
 
   private final HankDataStore output;
-  private final HankVersionType versionType;
+  private HankVersionType versionType;
   private final CoordinatorConfigurator configurator;
 
-  public HankDomainBuilderAction(String checkpointToken,
-                                 HankVersionType versionType,
-                                 CoordinatorConfigurator configurator,
-                                 HankDataStore output){
+  public HankDomainBuilderAction(
+      String checkpointToken,
+      CoordinatorConfigurator configurator,
+      HankDataStore output) {
+    this(checkpointToken, null, null, configurator, output);
+  }
+
+  public HankDomainBuilderAction(
+      String checkpointToken,
+      HankVersionType versionType,
+      CoordinatorConfigurator configurator,
+      HankDataStore output) {
     this(checkpointToken, null, versionType, configurator, output);
   }
 
@@ -43,8 +51,15 @@ public abstract class HankDomainBuilderAction extends Action {
   }
 
   public void execute() throws Exception {
+    preprocess();
+
+    if (getVersionType() == null) {
+      throw new IllegalStateException("Must set a version type before executing the domain builder!");
+    }
+
     DomainBuilderProperties domainBuilderProperties = new DomainBuilderProperties(
       output.getDomainName(), configurator, output.getPath());
+
     IncrementalDomainVersionProperties domainVersionProperties;
     switch (versionType) {
       case BASE:
@@ -57,13 +72,24 @@ public abstract class HankDomainBuilderAction extends Action {
       default:
         throw new RuntimeException("Unknown version type: " + versionType);
     }
+
     CascadingDomainBuilder builder = new CascadingDomainBuilder(domainBuilderProperties,
       domainVersionProperties, getPipe(), getKeyFieldName(), getValueFieldName());
+
     properties.putAll(CascadingHelper.DEFAULT_PROPERTIES);
     Flow flow = builder.build(properties, getSources());
+
     if (flow != null) {
       postProcessFlow(flow);
     }
+  }
+
+  protected void setVersionType(HankVersionType versionType) {
+    this.versionType = versionType;
+  }
+
+  protected HankVersionType getVersionType() {
+    return versionType;
   }
 
   protected abstract Pipe getPipe() throws Exception;
@@ -75,6 +101,10 @@ public abstract class HankDomainBuilderAction extends Action {
   protected abstract Map<String, Tap> getSources();
 
   protected void postProcessFlow(Flow flow) {
+    // Default is no-op
+  }
+
+  protected void preprocess() {
     // Default is no-op
   }
 }
