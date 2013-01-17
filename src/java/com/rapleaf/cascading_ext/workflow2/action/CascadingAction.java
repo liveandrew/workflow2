6db80495e8141eab390deb4ead52e48016c70e25
Ben Podgursky <bpodgursky@liveramp.com>
2013-01-17 14:46:15 -0800

    make even simpler

--- src/java/com/rapleaf/cascading_ext/workflow2/action/CascadingAction.java
+++ src/java/com/rapleaf/cascading_ext/workflow2/action/CascadingAction.java
@@ -8,15 +8,14 @@ import com.rapleaf.cascading_ext.CascadingHelper;
 import com.rapleaf.cascading_ext.datastore.DataStore;
 import com.rapleaf.cascading_ext.workflow2.Action;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public abstract class CascadingAction extends Action {
 
-  private Map<String, Tap> sources;
-  private Map<String, Tap> sinks;
+  private final Map<String, Tap> sources = new HashMap<String, Tap>();
+  private final Map<String, Tap> sinks = new HashMap<String, Tap>();
   private Map<Object, Object> flowProperties = Collections.emptyMap();
+  private List<Pipe> tails = new ArrayList<Pipe>();
 
   public CascadingAction(String checkpointToken,
                          List<DataStore> inputStores,
@@ -32,28 +31,51 @@ public abstract class CascadingAction extends Action {
     }
   }
 
-  protected void setSource(Tap source){
-    setSources(Collections.singletonMap("source", source));
+  protected void addTails(List<Pipe> tails) {
+    this.tails.addAll(tails);
   }
 
-  protected void setSources(Map<String, Tap> sources){
-    if(this.sources != null){
-      throw new RuntimeException("sources already set!");
+  protected void addTail(Pipe tail) {
+    this.tails.add(tail);
+  }
+
+  protected void addTails(Pipe ... tails){
+    Collections.addAll(this.tails, tails);
+  }
+
+  protected void addSource(Tap source){
+    addSource("singleton-source", source);
+  }
+
+  protected void addSources(Map<String, Tap> sources){
+    for(Map.Entry<String, Tap> source: sources.entrySet()){
+      addSource(source.getKey(), source.getValue());
+    }
+  }
+
+  protected void addSource(String name, Tap tap){
+    if(this.sources.containsKey(name)){
+      throw new RuntimeException("sources already contains name "+name+"!");
     }
 
-    this.sources = sources;
+    this.sources.put(name, tap);
+  }
+  protected void addSink(Tap sink){
+    addSink("singleton-sink", sink);
   }
 
-  protected void setSink(Tap sink){
-    setSinks(Collections.singletonMap("sink", sink));
+  protected void addSinks(Map<String, Tap> sinks){
+    for(Map.Entry<String, Tap> sink: sinks.entrySet()){
+      addSink(sink.getKey(), sink.getValue());
+    }
   }
 
-  protected void setSinks(Map<String, Tap> sinks){
-    if(this.sinks != null){
-      throw new RuntimeException("source already set!");
+  protected void addSink(String name, Tap sink){
+    if(this.sinks.containsKey(name)){
+      throw new RuntimeException("sinks already contains name "+name+"!");
     }
 
-    this.sinks = sinks;
+    this.sinks.put(name, sink);
   }
 
   protected void addFlowProperties(Map<Object, Object> properties){
@@ -62,7 +84,6 @@ public abstract class CascadingAction extends Action {
 
   @Override
   protected void execute() throws Exception {
-    List<Pipe> tails = getTails();
     String name = getClass().getSimpleName();
 
     FlowConnector connector = CascadingHelper.get().getFlowConnector(flowProperties);
@@ -86,6 +107,4 @@ public abstract class CascadingAction extends Action {
 
     completeWithProgress(f);
   }
-
-  protected abstract List<Pipe> getTails();
 }
