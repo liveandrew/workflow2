330f1879ecbf4950d9b041914ae4ae9575d7664b
Ben Podgursky <bpodgursky@liveramp.com>
2013-01-17 14:26:55 -0800

    Merge branch 'master' of git.liveramp.net:MasterRepos/cascading_ext

--- src/java/com/rapleaf/cascading_ext/workflow2/TrashHelper.java
+++ src/java/com/rapleaf/cascading_ext/workflow2/TrashHelper.java
@@ -10,9 +10,7 @@ import java.io.IOException;
 public class TrashHelper {
   public static void moveToTrash(FileSystem fs, Path path) throws IOException {
     Trash trash = new Trash(fs.getConf());
-    if (!trash.moveToTrash(path)) {
-      throw new RuntimeException("Trash disabled or path already in trash: "+path);
-    }
+    trash.moveToTrash(path);
   }
 
   public static boolean isEnabled() throws IOException {
