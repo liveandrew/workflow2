d60d736ff4c285dd6e97a6c54bfaa554f4e8166b
Ben Podgursky <bpodgursky@liveramp.com>
2013-01-17 16:37:40 -0800

    fix pathing

--- src/java/com/rapleaf/cascading_ext/workflow2/action/ScriptRunner.java
+++ src/java/com/rapleaf/cascading_ext/workflow2/action/ScriptRunner.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 
 public class ScriptRunner {
   public static void run(CascadingAction action) throws IOException {
-    String tmpPath = "/tmp/"+action.getClass().getSimpleName()+"/checkpoints";
+    String tmpPath = "/tmp/oneoff-script-runner/"+action.getCheckpointToken() +"/checkpoints";
 
     Step step = new Step(action);
     new WorkflowRunner("Oneoff runner: "+action.getClass().getSimpleName(), tmpPath, 1, 0, step)
