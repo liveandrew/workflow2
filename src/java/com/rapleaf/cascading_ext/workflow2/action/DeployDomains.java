2a904c58e94215d3432a94bfa39daa9b3ac55468
Porter Westling <pwestling@liveramp.com>
2013-04-23 11:51:31 -0700

    Merge pull request #45 from pwestling/master
    added DeployDomains action

---
+++ src/java/com/rapleaf/cascading_ext/workflow2/action/DeployDomains.java
@@ -0,0 +1,68 @@
+package com.rapleaf.cascading_ext.workflow2.action;
+
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
+import com.rapleaf.cascading_ext.datastore.HankDataStore;
+import com.rapleaf.cascading_ext.workflow2.Action;
+import com.rapleaf.hank.coordinator.Coordinator;
+import com.rapleaf.hank.coordinator.Domain;
+import com.rapleaf.hank.coordinator.DomainGroup;
+import com.rapleaf.hank.coordinator.DomainGroups;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class DeployDomains extends Action {
+
+  private final Coordinator coordinator;
+  private final List<String> domainNames;
+
+  public DeployDomains(String checkpointToken, Coordinator coordinator, String... domainNames) {
+    super(checkpointToken);
+    this.coordinator = coordinator;
+    this.domainNames = Lists.newArrayList(domainNames);
+  }
+
+  public DeployDomains(String checkpointToken, Coordinator coordinator, HankDataStore... domainStores) {
+    super(checkpointToken);
+    this.coordinator = coordinator;
+    domainNames = Lists.newArrayList();
+    for (HankDataStore store : domainStores) {
+      domainNames.add(store.getDomainName());
+    }
+  }
+
+  @Override
+  protected void execute() throws Exception {
+    HashMultimap<DomainGroup, Domain> updateMap = HashMultimap.create();
+
+    for (String domainName : domainNames) {
+      Domain domain = coordinator.getDomain(domainName);
+      Set<DomainGroup> domainGroups = getDomainGroups(domain);
+      for (DomainGroup domainGroup : domainGroups) {
+        updateMap.put(domainGroup, domain);
+      }
+    }
+
+    for (Map.Entry<DomainGroup, Collection<Domain>> entry : updateMap.asMap().entrySet()) {
+      DomainGroups.fastForwardDomains(entry.getKey(), entry.getValue());
+    }
+
+  }
+
+  private Set<DomainGroup> getDomainGroups(Domain domain) throws IOException {
+    Set<DomainGroup> domainGroups = coordinator.getDomainGroups();
+    Set<DomainGroup> relevantGroups = Sets.newHashSet();
+
+    for (DomainGroup group : domainGroups) {
+      if (group.getDomains().contains(domain)) {
+        relevantGroups.add(group);
+      }
+    }
+    return relevantGroups;
+  }
+}
