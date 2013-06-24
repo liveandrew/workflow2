21372031694bd0d7c90ad04325ce83e01d25cde3
Ben Podgursky <bpodgursky@liveramp.com>
2013-06-24 15:03:03 -0700

    Merge pull request #71 from MasterRepos/fixtrash
    check whether the server has trash enabled

--- src/java/com/rapleaf/cascading_ext/workflow2/TrashHelper.java
+++ src/java/com/rapleaf/cascading_ext/workflow2/TrashHelper.java
@@ -1,9 +1,11 @@
 package com.rapleaf.cascading_ext.workflow2;
 
+import com.liveramp.cascading_ext.FileSystemHelper;
 import com.rapleaf.cascading_ext.CascadingHelper;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.fs.Trash;
+import org.apache.hadoop.fs.TrashPolicyDefault;
 
 import java.io.IOException;
 
@@ -11,12 +13,23 @@ public class TrashHelper {
   public static void moveToTrash(FileSystem fs, Path path) throws IOException {
     Trash trash = new Trash(fs.getConf());
     if (!trash.moveToTrash(path)) {
-      throw new RuntimeException("Trash disabled or path already in trash: "+path);
+      throw new RuntimeException("Trash disabled or path already in trash: " + path);
     }
   }
 
   public static boolean isEnabled() throws IOException {
+
+    //  check if enabled locally
     Integer interval = Integer.parseInt(CascadingHelper.get().getJobConf().get("fs.trash.interval"));
-    return interval != 0;
+
+    if (interval != 0) {
+      return true;
+    }
+
+    //  it could also be configured on the namenode rather than locally, so look there
+    FileSystem fs = FileSystemHelper.getFS();
+    long trashInterval = fs.getServerDefaults(new Path("/tmp/some_dummy_path")).getTrashInterval();
+
+    return trashInterval != 0;
   }
 }
