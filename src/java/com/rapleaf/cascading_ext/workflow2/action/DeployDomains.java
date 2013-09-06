c22fc9094a3c39cd913d9099b3dd7002260d7440
Porter Westling <pwestling@liveramp.com>
2013-09-06 19:33:07 -0400

    add logging

--- src/java/com/rapleaf/cascading_ext/workflow2/action/DeployDomains.java
+++ src/java/com/rapleaf/cascading_ext/workflow2/action/DeployDomains.java
@@ -3,10 +3,6 @@ package com.rapleaf.cascading_ext.workflow2.action;
 import com.google.common.collect.HashMultimap;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
-import com.rapleaf.cascading_ext.datastore.HankDataStore;
-import com.rapleaf.cascading_ext.workflow2.Action;
-import com.rapleaf.cascading_ext.workflow2.Step;
-import com.rapleaf.cascading_ext.workflow2.WorkflowRunner;
 import com.liveramp.hank.config.CoordinatorConfigurator;
 import com.liveramp.hank.config.InvalidConfigurationException;
 import com.liveramp.hank.config.yaml.YamlClientConfigurator;
@@ -14,6 +10,12 @@ import com.liveramp.hank.coordinator.Coordinator;
 import com.liveramp.hank.coordinator.Domain;
 import com.liveramp.hank.coordinator.DomainGroup;
 import com.liveramp.hank.coordinator.DomainGroups;
+import com.liveramp.hank.coordinator.Domains;
+import com.rapleaf.cascading_ext.datastore.HankDataStore;
+import com.rapleaf.cascading_ext.workflow2.Action;
+import com.rapleaf.cascading_ext.workflow2.Step;
+import com.rapleaf.cascading_ext.workflow2.WorkflowRunner;
+import org.apache.log4j.Logger;
 
 import java.io.IOException;
 import java.util.Collection;
@@ -23,6 +25,7 @@ import java.util.Set;
 
 public class DeployDomains extends Action {
 
+  private final Logger LOG = Logger.getLogger(DeployDomains.class);
   private final Coordinator coordinator;
   private final List<String> domainNames;
 
@@ -58,9 +61,11 @@ public class DeployDomains extends Action {
     }
 
     for (Map.Entry<DomainGroup, Collection<Domain>> entry : updateMap.asMap().entrySet()) {
+      for (Domain domain : entry.getValue()) {
+        LOG.info("Fast Forwarding Domain :" + domain.getName() + " to version " + Domains.getLatestVersionNotOpenNotDefunct(domain).getVersionNumber());
+      }
       DomainGroups.fastForwardDomains(entry.getKey(), entry.getValue());
     }
-
   }
 
   private Set<DomainGroup> getDomainGroups(Domain domain) throws IOException {
