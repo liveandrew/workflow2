ef35e5e9a0546b5b064444c13b61daf1212013cd
Ben Podgursky <bpodgursky@liveramp.com>
2013-09-12 16:38:21 -0700

    Merge branch 'master' of git.liveramp.net:MasterRepos/cascading_ext

--- src/java/com/rapleaf/cascading_ext/workflow2/action/CleanUpOlderVersions.java
+++ src/java/com/rapleaf/cascading_ext/workflow2/action/CleanUpOlderVersions.java
@@ -4,9 +4,15 @@ import com.rapleaf.cascading_ext.datastore.DateVersionedBucketDataStore;
 import com.rapleaf.cascading_ext.datastore.VersionedBucketDataStore;
 import com.rapleaf.cascading_ext.workflow2.Action;
 
+import java.util.Arrays;
+
 public class CleanUpOlderVersions extends Action {
   private final int numVersionsToKeep;
   private final Iterable<? extends VersionedBucketDataStore> versionedDataStores;
+
+  public CleanUpOlderVersions(String checkpointToken, String tmpRoot, int numVersionsToKeep, VersionedBucketDataStore store) {
+    this(checkpointToken, tmpRoot, numVersionsToKeep, Arrays.asList(store));
+  }
 
   public CleanUpOlderVersions(String checkpointToken, String tmpRoot, int numVersionsToKeep, Iterable<? extends VersionedBucketDataStore> versionedDataStores) {
     super(checkpointToken, tmpRoot);
