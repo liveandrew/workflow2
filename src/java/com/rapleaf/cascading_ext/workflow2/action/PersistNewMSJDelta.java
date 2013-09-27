7fe652cded84888f41ad7105e6922b0a44381080
Porter Westling <pwestling@liveramp.com>
2013-09-27 16:22:32 -0700

    msj datastore action

---
+++ src/java/com/rapleaf/cascading_ext/workflow2/action/PersistNewMSJDelta.java
@@ -0,0 +1,26 @@
+package com.rapleaf.cascading_ext.workflow2.action;
+
+import com.rapleaf.cascading_ext.datastore.BucketDataStore;
+import com.rapleaf.cascading_ext.msj_tap.MSJDataStore;
+import com.rapleaf.cascading_ext.workflow2.Action;
+
+public class PersistNewMSJDelta<RecordType, KeyType extends Comparable> extends Action {
+
+  private final BucketDataStore versionToPersist;
+  private final MSJDataStore store;
+
+  public PersistNewMSJDelta(String checkpointToken, BucketDataStore<RecordType> versionToPersist, MSJDataStore<KeyType> store) {
+    super(checkpointToken);
+
+    this.versionToPersist = versionToPersist;
+    this.store = store;
+
+    readsFrom(versionToPersist);
+    writesTo(store);
+  }
+
+  @Override
+  protected void execute() throws Exception {
+    store.persistNewDelta(versionToPersist.getPath());
+  }
+}
