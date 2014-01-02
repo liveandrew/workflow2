27e32aef183252a54dfa19f8ba73d9782e5d095e
Ben Podgursky <bpodgursky@liveramp.com>
2014-01-02 09:37:55 -0600

    Merge branch 'master' of git.liveramp.net:MasterRepos/cascading_ext

--- src/java/com/rapleaf/cascading_ext/workflow2/action/AddMissingPartitionsToBucket.java
+++ src/java/com/rapleaf/cascading_ext/workflow2/action/AddMissingPartitionsToBucket.java
@@ -1,6 +1,8 @@
 package com.rapleaf.cascading_ext.workflow2.action;
 
+import com.liveramp.cascading_ext.FileSystemHelper;
 import com.rapleaf.cascading_ext.datastore.BucketDataStore;
+import com.rapleaf.cascading_ext.datastore.BucketDataStoreImpl;
 import com.rapleaf.cascading_ext.workflow2.Action;
 import com.rapleaf.formats.bucket.BucketUtil;
 
@@ -23,5 +25,12 @@ public class AddMissingPartitionsToBucket extends Action {
     dataStore.getBucket().markAsMutable();
     BucketUtil.addMissingPartitions(getFS(), dataStore.getPath(), numPartitions, recordClass);
     dataStore.getBucket().markAsImmutable();
+  }
+
+  public static void main(String[] args) throws Exception {
+    String path = args[0];
+    Class clazz = Class.forName(args[1]);
+    BucketDataStore dataStore = new BucketDataStoreImpl(FileSystemHelper.getFS(), "bucket", path, "", clazz);
+    new AddMissingPartitionsToBucket("checkpoint", 599, dataStore, clazz).execute();
   }
 }
