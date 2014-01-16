f5b856b6f0ffb78921d7ea5e6f3b4b67e342c3cc
matt <matt@liveramp.com>
2014-01-15 19:20:38 -0800

    Merge pull request #143 from MasterRepos/scale_counters_for_statsd
    Added scaling factors for counters sent to StatsD

--- src/java/com/rapleaf/cascading_ext/workflow2/StepStatsRecorder.java
+++ src/java/com/rapleaf/cascading_ext/workflow2/StepStatsRecorder.java
@@ -2,6 +2,7 @@ package com.rapleaf.cascading_ext.workflow2;
 
 import cascading.flow.SliceCounters;
 import cascading.flow.StepCounters;
+import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Lists;
 import com.rapleaf.cascading_ext.counters.NestedCounter;
 import com.rapleaf.support.Rap;
@@ -11,6 +12,7 @@ import org.apache.hadoop.mapreduce.TaskCounter;
 import org.apache.log4j.Logger;
 
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -32,6 +34,30 @@ class StatsDRecorder implements StepStatsRecorder {
   private StatsDClient client;
   private static final Logger LOG = Logger.getLogger(StatsDRecorder.class);
 
+  private static final int TIME_MS_SCALE = 100 * 1000;
+  private static final int BYTES_SCALE = 1000 * 1000;
+  private static final int RECORDS_SCALE = 100 * 1000;
+  private static final Map<String, Integer> COUNTER_SCALES = ImmutableMap.<String, Integer>builder()
+      .put("COMMITTED_HEAP_BYTES", BYTES_SCALE)
+      .put("PHYSICAL_MEMORY_BYTES", BYTES_SCALE)
+      .put("MAP_OUTPUT_BYTES", BYTES_SCALE)
+      .put("VIRTUAL_MEMORY_BYTES", BYTES_SCALE)
+
+      .put("MAP_INPUT_RECORDS", RECORDS_SCALE)
+      .put("MAP_OUTPUT_RECORDS", RECORDS_SCALE)
+      .put("REDUCE_INPUT_RECORDS", RECORDS_SCALE)
+      .put("REDUCE_OUTPUT_RECORDS", RECORDS_SCALE)
+
+      .put("Tuples_Read", RECORDS_SCALE)
+      .put("Tuples_Written", RECORDS_SCALE)
+
+      .put("CPU_MILLISECONDS", TIME_MS_SCALE)
+      .put("SLOTS_MILLIS_MAPS", TIME_MS_SCALE)
+      .put("SLOTS_MILLIS_REDUCE", TIME_MS_SCALE)
+      .put("FALLOW_SLOTS_MILLIS_MAPS", TIME_MS_SCALE)
+      .put("FALLOW_SLOTS_MILLIS_REDUCE", TIME_MS_SCALE)
+      .build();
+
   public StatsDRecorder(StatsDClient client) {
     this.client = client;
   }
@@ -45,6 +71,7 @@ class StatsDRecorder implements StepStatsRecorder {
   public void recordStats(Step step, Step.StepTimer timer) {
     try {
       String bucket = step.getCheckpointToken().replaceAll("__", "."); //Change MSA checkpoint notation to form buckets with substeps of MSAs
+      client.count(bucket + ".countRuns", 1);
 
       long milliTime = timer.getEventEndTime() - timer.getEventStartTime();
       int secondTime = Rap.safeLongToInt(TimeUnit.MILLISECONDS.toSeconds(milliTime));
@@ -68,7 +95,7 @@ class StatsDRecorder implements StepStatsRecorder {
     for (NestedCounter nestedCounter : step.getCounters()) {
       if (groupName.equals(nestedCounter.getCounter().getGroup()) &&
           counterName.equals(nestedCounter.getCounter().getName())) {
-        int value = value(nestedCounter);
+        int value = value(nestedCounter, counterName);
         client.count(bucket + "." + counterName, value);
         LOG.info("Recording " + groupName + ":" + counterName + "  :  " + value);
         return;
@@ -76,8 +103,11 @@ class StatsDRecorder implements StepStatsRecorder {
     }
   }
 
-  private int value(NestedCounter nestedCounter) {
-    return nestedCounter.getCounter().getValue().intValue();
+  private int value(NestedCounter nestedCounter, String counterName) {
+    final Integer objScale = COUNTER_SCALES.get(counterName);
+    final int scale = objScale == null ? 1 : objScale;
+    final long value = nestedCounter.getCounter().getValue();
+    return new Long(value / (long) scale).intValue();
   }
 }
 
