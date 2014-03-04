8926dcf1b3ad86faa482c768366fa28709e289c6
ytan <ytan@liveramp.com>
2014-03-04 15:25:21 -0800

    Merge pull request #151 from ytan/master
    add WorkflowRunnable class

---
+++ src/java/com/rapleaf/cascading_ext/workflow2/WorkflowRunnable.java
@@ -0,0 +1,22 @@
+package com.rapleaf.cascading_ext.workflow2;
+
+import java.io.IOException;
+
+//because workflowRunner is a final class, can't be mocked
+// make a workflowRunable for mockery test
+public class WorkflowRunnable implements Runnable {
+  private final WorkflowRunner runner;
+
+  public WorkflowRunnable(WorkflowRunner runner) {
+    this.runner = runner;
+  }
+
+  @Override
+  public void run() {
+    try {
+      runner.run();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+}
