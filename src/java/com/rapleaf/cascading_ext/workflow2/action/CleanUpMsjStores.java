45541f638ea5f31731ff61d88f6b7e2bb40674d8
piotr <piotr@liveramp.com>
2014-07-22 19:01:21 -0700

    added action to clean up old versions

--- src/java/com/rapleaf/cascading_ext/workflow2/action/CleanUpMsjStores.java
+++ src/java/com/rapleaf/cascading_ext/workflow2/action/CleanUpMsjStores.java
@@ -1,9 +1,16 @@
 package com.rapleaf.cascading_ext.workflow2.action;
 
+import java.util.Iterator;
 import java.util.List;
+import java.util.SortedSet;
 
 import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
 
+import com.liveramp.cascading_ext.FileSystemHelper;
 import com.rapleaf.cascading_ext.msj_tap.compaction.Compactor;
 import com.rapleaf.cascading_ext.msj_tap.store.MSJDataStore;
 import com.rapleaf.cascading_ext.msj_tap.store.TMSJDataStore;
@@ -30,12 +37,47 @@ public class CleanUpMsjStores extends MultiStepAction {
     }
   }
 
-  public CleanUpMsjStores(String checkpointToken, TMSJDataStore... stores) {
+  private class DeleteOldVersions extends Action {
+
+    private final int numVersionsToKeep;
+    private final MSJDataStore store;
+
+    private DeleteOldVersions(MSJDataStore store, int numVersionsToKeep) {
+      super("compact_" + store.getPath().replaceAll("/", "_"));
+      this.numVersionsToKeep = numVersionsToKeep;
+      this.store = store;
+      if (numVersionsToKeep < 1) {
+        throw new IllegalArgumentException("At least one version must be kept");
+      }
+    }
+
+    @Override
+    protected void execute() throws Exception {
+      FileSystem fs = FileSystemHelper.getFS();
+      FileStatus[] statuses = fs.listStatus(new Path(store.getPath()));
+      SortedSet<Integer> versions = Sets.newTreeSet();
+      for (FileStatus status : statuses) {
+        String dirName = status.getPath().getName();
+        versions.add(Integer.parseInt(dirName.split("_")[1]));
+      }
+      Iterator<Integer> iterator = versions.iterator();
+      for (int i = 0; i < versions.size() - numVersionsToKeep; i++) {
+        Integer version = iterator.next();
+        fs.delete(new Path(store.getPath() + "/base_" + version), true);
+        fs.delete(new Path(store.getPath() + "/mailbox_" + version), true);
+      }
+    }
+  }
+
+  public CleanUpMsjStores(String checkpointToken, int numVersionsToKeep, boolean runCompaction, TMSJDataStore... stores) {
     super(checkpointToken);
 
     List<Step> steps = Lists.newArrayList();
     for (TMSJDataStore store : stores) {
-      steps.add(new Step(new MSJDataStoreCompactionAction(store)));
+      if (runCompaction) {
+        steps.add(new Step(new MSJDataStoreCompactionAction(store)));
+      }
+      steps.add(new Step(new DeleteOldVersions(store, numVersionsToKeep)));
     }
 
     setSubStepsFromTails(steps);
---
+++ test/java/com/rapleaf/cascading_ext/workflow2/action/TestCleanUpMsjStores.java
@@ -0,0 +1,62 @@
+package com.rapleaf.cascading_ext.workflow2.action;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Set;
+
+import com.google.common.collect.Sets;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.Path;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.rapleaf.cascading_ext.CascadingExtTestCase;
+import com.rapleaf.cascading_ext.datastore.TMSJDataStoreHelper;
+import com.rapleaf.cascading_ext.map_side_join.extractors.TBinaryExtractor;
+import com.rapleaf.cascading_ext.msj_tap.store.TMSJDataStore;
+import com.rapleaf.types.new_person_data.DustinInternalEquiv;
+import com.rapleaf.types.new_person_data.PIN;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestCleanUpMsjStores extends CascadingExtTestCase {
+
+  private TMSJDataStore<DustinInternalEquiv> store;
+
+  @Before
+  public void setUp() throws Exception {
+    store = new TMSJDataStore<DustinInternalEquiv>(getTestRoot() + "/store",
+        DustinInternalEquiv.class,
+        new TBinaryExtractor<PIN>(PIN.class, DustinInternalEquiv._Fields.PIN),
+        0.5);
+  }
+
+  @Test
+  public void testDeleteOldVersions() throws Exception {
+    writeNewVersions(5);
+    runAndVerify(2, new int[] {3, 4});
+    runAndVerify(2, new int[] {3, 4});
+    runAndVerify(5, new int[] {3, 4});
+    writeNewVersions(5);
+    runAndVerify(3, new int[] {7, 8, 9});
+  }
+
+  private void runAndVerify(int numToKeep, int[] expectedRemaining) throws IOException {
+    executeWorkflow(new CleanUpMsjStores("checkpoint", numToKeep, false, store));
+    FileStatus[] statuses = getFS().listStatus(new Path(store.getPath()));
+    Set<String> remaining = Sets.newHashSet();
+    for (FileStatus status : statuses) {
+      remaining.add(status.getPath().getName());
+    }
+    assertEquals("Total bases and mailboxes should match expected values", 2 * expectedRemaining.length, remaining.size());
+  }
+
+  private void writeNewVersions(int numVersions) throws IOException, org.apache.thrift.TException {
+    for (int i = 0; i < numVersions; i++) {
+      if (store.getStore().hasBase()) {
+        store.getStore().acquireBaseCreationAttempt();
+      }
+      TMSJDataStoreHelper.writeNewBase(builder(), store, new ArrayList<DustinInternalEquiv>());
+    }
+  }
+}
