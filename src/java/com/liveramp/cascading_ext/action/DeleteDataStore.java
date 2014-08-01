a3e0fd09a1b11aae521425beec153e9237f6951a
matt <matt@liveramp.com>
2014-07-31 17:55:36 -0700

    Merge pull request #169 from MasterRepos/delete_bucket
    Created an action to delete a DataStore

---
+++ src/java/com/liveramp/cascading_ext/action/DeleteDataStore.java
@@ -0,0 +1,31 @@
+package com.liveramp.cascading_ext.action;
+
+
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+
+import com.liveramp.cascading_ext.FileSystemHelper;
+import com.rapleaf.cascading_ext.datastore.DataStore;
+import com.rapleaf.cascading_ext.workflow2.Action;
+
+public class DeleteDataStore extends Action {
+
+  private final FileSystem fs;
+  private final DataStore dataStore;
+
+  public DeleteDataStore(String checkpointToken, FileSystem fs, DataStore dataStore) {
+    super(checkpointToken);
+    this.fs = fs;
+    this.dataStore = dataStore;
+  }
+
+  public DeleteDataStore(String checkpointToken, DataStore dataStore) {
+    this(checkpointToken, FileSystemHelper.getFS(), dataStore);
+  }
+
+  @SuppressWarnings("PMD.BlacklistedMethods")
+  @Override
+  protected void execute() throws Exception {
+    fs.delete(new Path(dataStore.getPath()), true);
+  }
+}
