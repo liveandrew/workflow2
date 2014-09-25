497484e274b26826476f9c05643ce925877d7aaf
Ben Podgursky <bpodgursky@liveramp.com>
2014-09-25 09:07:23 -0700

    Merge pull request #206 from MasterRepos/partitioned_data_store_delete_old_versions
    PartitionedDataStore deletes old versions

--- src/java/com/rapleaf/cascading_ext/workflow2/action/PersistNewPartitionedVersion.java
+++ src/java/com/rapleaf/cascading_ext/workflow2/action/PersistNewPartitionedVersion.java
@@ -2,6 +2,7 @@ package com.rapleaf.cascading_ext.workflow2.action;
 
 import java.io.IOException;
 
+import com.google.common.collect.ListMultimap;
 import org.apache.thrift.TBase;
 
 import com.rapleaf.cascading_ext.datastore.PartitionedDataStore;
@@ -13,19 +14,24 @@ public class PersistNewPartitionedVersion<T extends TBase> extends Action {
   private final PartitionedDataStore destinationStore;
   private final PartitionedDataStore sourceStore;
   private final Class klass;
+  private final ListMultimap<Long, Integer> persistedVersions;
 
-  public PersistNewPartitionedVersion(String checkpointToken, String tmpRoot, Class klass,
+  public PersistNewPartitionedVersion(String checkpointToken,
+                                      String tmpRoot,
+                                      Class klass,
                                       PartitionedDataStore<? extends T> sourceStore,
-                                      PartitionedDataStore<? extends T> destinationStore) throws IOException {
+                                      PartitionedDataStore<? extends T> destinationStore,
+                                      ListMultimap<Long, Integer> persistedVersions) throws IOException {
     super(checkpointToken, tmpRoot);
 
     this.destinationStore = destinationStore;
     this.sourceStore = sourceStore;
     this.klass = klass;
+    this.persistedVersions = persistedVersions;
   }
 
   @Override
   protected void execute() throws Exception {
-    destinationStore.persistFrom(sourceStore);
+    destinationStore.persistFrom(sourceStore, persistedVersions);
   }
 }
