280134418b71e6505558e12af7435ed3929d6132
Ben Podgursky <bpodgursky@liveramp.com>
2014-09-30 19:00:50 -0700

    Merge branch 'master' of git.liveramp.net:MasterRepos/cascading_ext

---
+++ src/java/com/rapleaf/cascading_ext/workflow2/action/CommitMSJDelete.java
@@ -0,0 +1,25 @@
+package com.rapleaf.cascading_ext.workflow2.action;
+
+import com.rapleaf.cascading_ext.datastore.BucketDataStore;
+import com.rapleaf.cascading_ext.msj_tap.store.MSJDataStore;
+import com.rapleaf.cascading_ext.workflow2.Action;
+
+public class CommitMSJDelete<RecordType, KeyType extends Comparable> extends Action {
+  private final BucketDataStore<RecordType> versionToCommit;
+  private final MSJDataStore<KeyType> store;
+
+  public CommitMSJDelete(String checkpointToken, BucketDataStore<RecordType> versionToCommit, MSJDataStore<KeyType> store) {
+    super(checkpointToken);
+
+    this.versionToCommit = versionToCommit;
+    this.store = store;
+
+    readsFrom(versionToCommit);
+    writesTo(store);
+  }
+
+  @Override
+  protected void execute() throws Exception {
+    store.commitDelete(versionToCommit.getPath());
+  }
+}
---
+++ test/java/com/rapleaf/cascading_ext/workflow2/action/TestTMSJDelete.java
@@ -0,0 +1,79 @@
+package com.rapleaf.cascading_ext.workflow2.action;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.List;
+
+import com.google.common.collect.Lists;
+import org.apache.thrift.TException;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.rapleaf.cascading_ext.CascadingExtTestCase;
+import com.rapleaf.cascading_ext.HRap;
+import com.rapleaf.cascading_ext.datastore.BucketDataStore;
+import com.rapleaf.cascading_ext.datastore.TMSJDataStoreHelper;
+import com.rapleaf.cascading_ext.map_side_join.extractors.TStringBytesExtractor;
+import com.rapleaf.cascading_ext.msj_tap.store.TMSJDataStore;
+import com.rapleaf.cascading_ext.test.ByteArrayExtractorComparator;
+import com.rapleaf.cascading_ext.workflow2.Step;
+import com.rapleaf.formats.test.ThriftBucketHelper;
+import com.rapleaf.types.new_person_data.SPEL;
+
+public class TestTMSJDelete extends CascadingExtTestCase {
+  private TMSJDataStore<SPEL> spelDataStore;
+  private BucketDataStore<SPEL> deletionDelta;
+
+  @Before
+  public void prepareDataStore() throws IOException, TException {
+    spelDataStore = new TMSJDataStore<SPEL>(getTestRoot() + "/spel_data_store",
+        SPEL.class,
+        new TStringBytesExtractor(SPEL._Fields.SPEL),
+        .2,
+        3
+    );
+
+    TMSJDataStoreHelper.writeNewBase(builder(), spelDataStore, Lists.newArrayList(
+        new SPEL("1"),
+        new SPEL("3"),
+        new SPEL("4"),
+        new SPEL("5")
+    ));
+
+    TMSJDataStoreHelper.writeNewDelta(builder(), spelDataStore, Lists.newArrayList(
+        new SPEL("2"),
+        new SPEL("4")
+    ));
+
+    deletionDelta = builder().getBucketDataStore("deletion_Data", SPEL.class);
+    ThriftBucketHelper.writeToBucketAndSort(
+        deletionDelta.getBucket(),
+        new ByteArrayExtractorComparator<SPEL>(SPEL._Fields.SPEL),
+        Lists.newArrayList(
+            new SPEL("1"),
+            new SPEL("4")
+        )
+    );
+  }
+
+  @Test
+  public void testIt() throws IOException {
+    Step delete = new Step(new CommitTMSJDelete<SPEL>(
+        "commit-delete",
+        deletionDelta,
+        spelDataStore
+    ));
+
+    executeWorkflow(delete);
+
+    List<SPEL> expected = Lists.newArrayList(
+        new SPEL("2"),
+        new SPEL("3"),
+        new SPEL("5")
+    );
+
+    Collection<SPEL> actual = HRap.getValues(spelDataStore);
+
+    assertCollectionEquivalent(expected, actual);
+  }
+}
