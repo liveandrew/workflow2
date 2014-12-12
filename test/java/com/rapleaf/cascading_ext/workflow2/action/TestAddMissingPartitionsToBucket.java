f055aa04711fcaa335034e26856492ec58c9cf8a
Roshan George <rgeorge@liveramp.com>
2014-12-11 17:46:35 -0800

    Generify AddMissingPartitions

--- test/java/com/rapleaf/cascading_ext/workflow2/action/TestAddMissingPartitionsToBucket.java
+++ test/java/com/rapleaf/cascading_ext/workflow2/action/TestAddMissingPartitionsToBucket.java
@@ -1,18 +1,19 @@
 package com.rapleaf.cascading_ext.workflow2.action;
 
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
 import com.google.common.collect.Sets;
+import org.apache.hadoop.io.BytesWritable;
+import org.junit.Test;
+
 import com.rapleaf.cascading_ext.CascadingExtTestCase;
 import com.rapleaf.cascading_ext.datastore.BucketDataStore;
 import com.rapleaf.cascading_ext.datastore.internal.DataStoreBuilder;
 import com.rapleaf.formats.bucket.Bucket;
 import com.rapleaf.formats.stream.RecordOutputStream;
 import com.rapleaf.support.Strings;
-import org.apache.hadoop.io.BytesWritable;
-import org.junit.Test;
-
-import java.io.IOException;
-import java.util.HashSet;
-import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
 
@@ -20,12 +21,12 @@ public class TestAddMissingPartitionsToBucket extends CascadingExtTestCase {
 
   @Test
   public void testIt() throws Exception {
-    BucketDataStore dataStore = new DataStoreBuilder(getTestRoot()).getBytesDataStore("store");
+    BucketDataStore<BytesWritable> dataStore = new DataStoreBuilder(getTestRoot()).getBytesDataStore("store");
     Bucket bucket = Bucket.open(fs, dataStore.getBucket().getRoot().toString(), BytesWritable.class);
     write(bucket, "part-00012_0", "a", "b", "c");
     write(bucket, "part-00061_0", "d", "e", "f");
-    new AddMissingPartitionsToBucket("add-missing-partitions", 70, dataStore, BytesWritable.class).execute();
-    assertEquals(Sets.<String>newHashSet("a", "b", "c", "d", "e", "f"), readRecords(bucket));
+    new AddMissingPartitionsToBucket<BytesWritable>("add-missing-partitions", 70, dataStore, BytesWritable.class).execute();
+    assertEquals(Sets.newHashSet("a", "b", "c", "d", "e", "f"), readRecords(bucket));
     assertEquals(70, bucket.getStoredFiles().length);
   }
 
