2a1683ffb933f4aead35aafe0dfd02a4cae10489
Tenzing Shaw <tshaw@liveramp.com>
2015-01-03 00:07:37 -0800

    Revert "assert production in WorkflowRunnerOptions"
    This reverts commit 350f221c82e625265b1ec7910f4da35b7f48b448.

--- src/java/com/rapleaf/cascading_ext/workflow2/WorkflowRunnerOptions.java
+++ src/java/com/rapleaf/cascading_ext/workflow2/WorkflowRunnerOptions.java
@@ -4,6 +4,7 @@ import org.apache.log4j.Logger;
 
 import com.liveramp.java_support.alerts_handler.LoggingAlertsHandler;
 import com.rapleaf.cascading_ext.workflow2.options.WorkflowOptions;
+import com.rapleaf.cascading_ext.workflow2.registry.MockRegistry;
 import com.rapleaf.cascading_ext.workflow2.registry.ZkRegistry;
 import com.rapleaf.cascading_ext.workflow2.stats.RecorderFactory;
 import com.rapleaf.support.Rap;
@@ -15,16 +16,28 @@ public class WorkflowRunnerOptions extends WorkflowOptions<WorkflowRunnerOptions
 
   public WorkflowRunnerOptions() {
 
-    Rap.assertProduction();
+    //  TODO get rid of this block once we don't use this classs in test (few OL things still do
+    if(Rap.getTestMode()){
+      LOG.warn("Should not use WorkflowRunnerOptions in test!  Use TestWorkflowOptions");
 
-    setMaxConcurrentSteps(Integer.MAX_VALUE);
-    setAlertsHandler(new LoggingAlertsHandler());
-    setEnabledNotifications(WorkflowRunnerNotificationSet.all());
-    setStatsRecorder(new RecorderFactory.StatsD());
-    setLockProvider(null);
-    setStorage(new ContextStorage.None());
-    setRegistry(new ZkRegistry());
+      setMaxConcurrentSteps(1);
+      setAlertsHandler(new LoggingAlertsHandler());
+      setEnabledNotifications(WorkflowRunnerNotificationSet.all());
+      setStatsRecorder(new RecorderFactory.Mock());
+      setLockProvider(null);
+      setStorage(new ContextStorage.None());
+      setRegistry(new MockRegistry());
+    }
 
+    else {
+      setMaxConcurrentSteps(Integer.MAX_VALUE);
+      setAlertsHandler(new LoggingAlertsHandler());
+      setEnabledNotifications(WorkflowRunnerNotificationSet.all());
+      setStatsRecorder(new RecorderFactory.StatsD());
+      setLockProvider(null);
+      setStorage(new ContextStorage.None());
+      setRegistry(new ZkRegistry());
+    }
   }
 
 }
