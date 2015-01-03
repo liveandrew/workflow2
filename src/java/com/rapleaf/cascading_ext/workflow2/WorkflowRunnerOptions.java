22532451fd670f8f504b207a7589a981a5fa60ac
Tenzing Shaw <tshaw@liveramp.com>
2015-01-02 20:25:11 -0800

    Merge branch 'workflow_runner_options_only_in_production' of git.liveramp.net:MasterRepos/cascading_ext

--- src/java/com/rapleaf/cascading_ext/workflow2/WorkflowRunnerOptions.java
+++ src/java/com/rapleaf/cascading_ext/workflow2/WorkflowRunnerOptions.java
@@ -4,7 +4,6 @@ import org.apache.log4j.Logger;
 
 import com.liveramp.java_support.alerts_handler.LoggingAlertsHandler;
 import com.rapleaf.cascading_ext.workflow2.options.WorkflowOptions;
-import com.rapleaf.cascading_ext.workflow2.registry.MockRegistry;
 import com.rapleaf.cascading_ext.workflow2.registry.ZkRegistry;
 import com.rapleaf.cascading_ext.workflow2.stats.RecorderFactory;
 import com.rapleaf.support.Rap;
@@ -16,28 +15,16 @@ public class WorkflowRunnerOptions extends WorkflowOptions<WorkflowRunnerOptions
 
   public WorkflowRunnerOptions() {
 
-    //  TODO get rid of this block once we don't use this classs in test (few OL things still do
-    if(Rap.getTestMode()){
-      LOG.warn("Should not use WorkflowRunnerOptions in test!  Use TestWorkflowOptions");
+    Rap.assertProduction();
 
-      setMaxConcurrentSteps(1);
-      setAlertsHandler(new LoggingAlertsHandler());
-      setEnabledNotifications(WorkflowRunnerNotificationSet.all());
-      setStatsRecorder(new RecorderFactory.Mock());
-      setLockProvider(null);
-      setStorage(new ContextStorage.None());
-      setRegistry(new MockRegistry());
-    }
+    setMaxConcurrentSteps(Integer.MAX_VALUE);
+    setAlertsHandler(new LoggingAlertsHandler());
+    setEnabledNotifications(WorkflowRunnerNotificationSet.all());
+    setStatsRecorder(new RecorderFactory.StatsD());
+    setLockProvider(null);
+    setStorage(new ContextStorage.None());
+    setRegistry(new ZkRegistry());
 
-    else {
-      setMaxConcurrentSteps(Integer.MAX_VALUE);
-      setAlertsHandler(new LoggingAlertsHandler());
-      setEnabledNotifications(WorkflowRunnerNotificationSet.all());
-      setStatsRecorder(new RecorderFactory.StatsD());
-      setLockProvider(null);
-      setStorage(new ContextStorage.None());
-      setRegistry(new ZkRegistry());
-    }
   }
 
 }
