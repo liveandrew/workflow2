9436698d8679de2c71b36e762554cc2b17a04cf7
Roshan George <rgeorge@liveramp.com>
2015-02-24 14:54:28 -0800

    Deprecate another usage of MSA with null tmproot

--- src/java/com/rapleaf/cascading_ext/workflow2/MultiStepAction.java
+++ src/java/com/rapleaf/cascading_ext/workflow2/MultiStepAction.java
@@ -27,6 +27,7 @@ public class MultiStepAction extends Action {
     }
   }
 
+  @Deprecated
   public MultiStepAction(String checkpointToken, Collection<Step> steps) {
     this(checkpointToken, null, steps);
   }
