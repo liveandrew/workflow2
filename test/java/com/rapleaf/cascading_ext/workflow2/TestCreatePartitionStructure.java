b4c626838c40e13d10c40dc1f7f5ba105b01c81d
Ben Podgursky <bpodgursky@liveramp.com>
2015-03-12 09:14:15 -0700

    Merge pull request #322 from MasterRepos/refactor_partitioner
    Clean up partitioner config interface and usage

--- test/java/com/rapleaf/cascading_ext/workflow2/TestCreatePartitionStructure.java
+++ test/java/com/rapleaf/cascading_ext/workflow2/TestCreatePartitionStructure.java
@@ -1,12 +1,8 @@
 package com.rapleaf.cascading_ext.workflow2;
 
 import java.io.IOException;
-import java.util.Collections;
 
-import com.google.common.collect.HashMultimap;
 import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
-import com.google.common.collect.Multimap;
 import com.google.common.collect.Sets;
 import junit.framework.Assert;
 import org.apache.hadoop.fs.LocatedFileStatus;
@@ -14,16 +10,20 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.fs.RemoteIterator;
 import org.apache.hadoop.io.BytesWritable;
 import org.junit.Test;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import com.liveramp.cascading_ext.FileSystemHelper;
+import com.liveramp.commons.collections.list.ListBuilder;
 import com.liveramp.commons.collections.map.MapBuilder;
 import com.liveramp.util.generated.StringOrNone;
 import com.rapleaf.cascading_ext.CascadingExtTestCase;
-import com.rapleaf.cascading_ext.datastore.PartitionedAudienceDatastore;
 import com.rapleaf.cascading_ext.datastore.PartitionedDataStore;
 import com.rapleaf.cascading_ext.map_side_join.extractors.TByteArrayExtractor;
+import com.rapleaf.cascading_ext.map_side_join.partitioning.IdentityAggregator;
+import com.rapleaf.cascading_ext.map_side_join.partitioning.PartitionerConfig;
 import com.rapleaf.cascading_ext.msj_tap.conf.InputConf;
 import com.rapleaf.cascading_ext.tap.bucket2.PartitionStructure;
+import com.rapleaf.cascading_ext.tap.bucket2.partitioner.AudienceVersion;
 import com.rapleaf.cascading_ext.workflow2.action.CreatePartitionStructure;
 
 public class TestCreatePartitionStructure extends CascadingExtTestCase {
@@ -37,11 +37,36 @@ public class TestCreatePartitionStructure extends CascadingExtTestCase {
         StringOrNone.class
     );
 
-    PartitionStructure structure = PartitionedAudienceDatastore.fromSplitMemberVersions(MapBuilder.of(10l, 1).put(20l, 2).get());
+    PartitionerConfig<StringOrNone, StringOrNone, AudienceVersion> partitioner = new PartitionerConfig<StringOrNone, StringOrNone, AudienceVersion>(1, StringOrNone.class, new IdentityAggregator<StringOrNone>()) {
+      @Override
+      public byte[] getKey(StringOrNone value) {
+        throw new NotImplementedException();
+      }
+
+      @Override
+      public AudienceVersion getCategory(StringOrNone value) {
+        throw new NotImplementedException();
+      }
+
+      @Override
+      public String getGroupPrefix(AudienceVersion category) {
+        return Long.toString(category.getAudience());
+      }
+
+      @Override
+      public String getSplit(AudienceVersion category) {
+        return category.getAudience() + "/" + category.getVersion();
+      }
+    };
+
+    PartitionStructure struct = partitioner.getExpectedPartitions(new ListBuilder<AudienceVersion>()
+        .add(new AudienceVersion(10l, 1))
+        .add(new AudienceVersion(20l, 2))
+        .get());
 
     CreatePartitionStructure action = new CreatePartitionStructure(
         "create",
-        structure,
+        struct,
         store
     );
 
