9e643397839f868fd5f336cdd04fc6981128f5cc
Ben Podgursky <bpodgursky@liveramp.com>
2015-03-19 11:13:50 -0700

    Revert "allow splitting to be injected into msj tap action"
    This reverts commit f295da592e8d04c390d3529a0412bcef6c79fd8b.

--- src/java/com/rapleaf/cascading_ext/workflow2/action/MSJTapAction.java
+++ src/java/com/rapleaf/cascading_ext/workflow2/action/MSJTapAction.java
@@ -7,27 +7,19 @@ import java.util.Map;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 
-import cascading.flow.FlowProcess;
-import cascading.operation.BaseOperation;
-import cascading.operation.Function;
-import cascading.operation.FunctionCall;
 import cascading.pipe.Each;
 import cascading.pipe.Pipe;
 import cascading.tap.Tap;
-import cascading.tuple.Fields;
-import cascading.tuple.Tuple;
 
 import com.rapleaf.cascading_ext.datastore.DataStore;
+import com.rapleaf.cascading_ext.msj_tap.InsertEmptySplit;
 import com.rapleaf.cascading_ext.msj_tap.conf.InputConf;
 import com.rapleaf.cascading_ext.msj_tap.operation.MSJFunction;
 import com.rapleaf.cascading_ext.msj_tap.scheme.MSJScheme;
 import com.rapleaf.cascading_ext.msj_tap.store.PartitionableDataStore;
 import com.rapleaf.cascading_ext.msj_tap.tap.MSJTap;
 import com.rapleaf.cascading_ext.tap.bucket2.PartitionStructure;
-import com.rapleaf.cascading_ext.tap.bucket2.PartitionedBucketScheme;
-import com.rapleaf.cascading_ext.tap.bucket2.ThriftBucketScheme;
 import com.rapleaf.cascading_ext.workflow2.CascadingAction2;
-import com.rapleaf.cascading_ext.workflow2.PartitionFactory;
 import com.rapleaf.cascading_ext.workflow2.TapFactory;
 
 public class MSJTapAction<K extends Comparable> extends CascadingAction2 {
@@ -47,36 +39,6 @@ public class MSJTapAction<K extends Comparable> extends CascadingAction2 {
                       MSJFunction<K> function,
                       PartitionableDataStore output,
                       PartitionStructure outputStructure) {
-    this(checkpointToken, tmpRoot, properties, inputs, function, new SplitGenerator.Empty(), output, outputStructure);
-  }
-
-
-  public MSJTapAction(String checkpointToken, String tmpRoot,
-                      final ExtractorsList<K> inputs,
-                      MSJFunction<K> function,
-                      SplitGenerator splitGen,
-                      PartitionableDataStore output,
-                      PartitionStructure outputStructure) {
-    this(checkpointToken, tmpRoot, Maps.newHashMap(), inputs, function, splitGen, output, new PartitionFactory.Now(outputStructure));
-  }
-
-  public MSJTapAction(String checkpointToken, String tmpRoot,
-                      Map<Object, Object> properties,
-                      final ExtractorsList<K> inputs,
-                      MSJFunction<K> function,
-                      SplitGenerator splitGen,
-                      PartitionableDataStore output,
-                      PartitionStructure outputStructure) {
-    this(checkpointToken, tmpRoot, properties, inputs, function, splitGen, output, new PartitionFactory.Now(outputStructure));
-  }
-
-  public MSJTapAction(String checkpointToken, String tmpRoot,
-                      Map<Object, Object> properties,
-                      final ExtractorsList<K> inputs,
-                      MSJFunction<K> function,
-                      SplitGenerator splitGen,
-                      PartitionableDataStore output,
-                      PartitionFactory structureFactory) {
     super(checkpointToken, tmpRoot, properties);
 
     final List<StoreExtractor<K>> asList = inputs.get();
@@ -98,27 +60,9 @@ public class MSJTapAction<K extends Comparable> extends CascadingAction2 {
         function
     );
 
-    pipe = new Each(pipe,
-        new Fields(ThriftBucketScheme.getFieldName(output.getRecordsType())),
-        new InsertSplit(splitGen));
+    pipe = new InsertEmptySplit(pipe);
 
-    completePartitioned("msj-tap", pipe, output, structureFactory);
-  }
-
-  private static class InsertSplit extends BaseOperation implements Function {
-
-    private final SplitGenerator generator;
-
-    public InsertSplit(SplitGenerator generator) {
-      super(new Fields(PartitionedBucketScheme.SPLIT_FIELD));
-      this.generator = generator;
-    }
-
-    @SuppressWarnings("unchecked")
-    @Override
-    public void operate(FlowProcess flowProcess, FunctionCall functionCall) {
-      functionCall.getOutputCollector().add(new Tuple(generator.getSplit(functionCall.getArguments().getObject(0))));
-    }
+    completePartitioned("msj-tap", pipe, output, outputStructure);
   }
 
   private List<InputConf<K>> getConfs(List<StoreExtractor<K>> inputs) throws IOException {
