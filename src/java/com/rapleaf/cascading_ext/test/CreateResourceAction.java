f6f01fe0aa13b5201684664d333e509eab555ac0
Roshan George <rgeorge@liveramp.com>
2015-04-05 14:32:35 -0700

    When you just need a resource for a test but can't find one

---
+++ src/java/com/rapleaf/cascading_ext/test/CreateResourceAction.java
@@ -0,0 +1,24 @@
+package com.rapleaf.cascading_ext.test;
+
+import com.rapleaf.cascading_ext.workflow2.Action;
+import com.rapleaf.cascading_ext.workflow2.OldResource;
+
+public class CreateResourceAction<T> extends Action {
+  private final OldResource<T> resource;
+  private final T value;
+
+  public CreateResourceAction(String checkpointToken, String tmpRoot,
+                              OldResource<T> resource,
+                              T value) {
+    super(checkpointToken, tmpRoot);
+    this.resource = resource;
+    this.value = value;
+
+    creates(resource);
+  }
+
+  @Override
+  protected void execute() throws Exception {
+    set(resource, value);
+  }
+}
