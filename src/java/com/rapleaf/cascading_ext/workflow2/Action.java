f78ae5a2e28764cdda0929465ef520d9a3ae4590
Soerian Lieve <slieve@liveramp.com>
2015-04-28 11:25:03 -0700

    Disallow calling execute() directly

--- src/java/com/rapleaf/cascading_ext/workflow2/Action.java
+++ src/java/com/rapleaf/cascading_ext/workflow2/Action.java
@@ -408,23 +408,10 @@ public abstract class Action {
   }
 
   protected FlowBuilder buildFlow(Map<Object, Object> properties) {
-    NestedProperties flowProperties;
-    //TODO Sweep direct calls to execute() so we don't have to do this!
-    if (nestedProperties != null) {
-      flowProperties = new NestedProperties(nestedProperties, properties);
-    } else {
-      flowProperties =
-          new NestedProperties(
-              new NestedProperties(
-                  new NestedProperties(
-                      null,
-                      CascadingHelper.get().getDefaultHadoopProperties()
-                  ),
-                  stepProperties
-              ),
-              properties
-          );
+    if (nestedProperties == null) {
+      throw new RuntimeException("Cannot call execute() directly!");
     }
+    NestedProperties flowProperties = new NestedProperties(nestedProperties, properties);
     return new FlowBuilder(buildFlowConnector(flowProperties.getPropertiesMap()), getClass());
   }
 
