b19cf20786f46768c1995ff84f76920ef67855bb
Ben Podgursky <bpodgursky@liveramp.com>
2015-08-06 14:35:03 -0700

    add option

--- src/java/com/rapleaf/cascading_ext/workflow2/action/MSJTapAction.java
+++ src/java/com/rapleaf/cascading_ext/workflow2/action/MSJTapAction.java
@@ -23,12 +23,13 @@ import com.rapleaf.cascading_ext.msj_tap.operation.MSJFunction;
 import com.rapleaf.cascading_ext.msj_tap.scheme.MSJScheme;
 import com.rapleaf.cascading_ext.msj_tap.store.PartitionableDataStore;
 import com.rapleaf.cascading_ext.msj_tap.tap.MSJTap;
+import com.rapleaf.cascading_ext.tap.TapFactory;
 import com.rapleaf.cascading_ext.tap.bucket2.PartitionStructure;
 import com.rapleaf.cascading_ext.tap.bucket2.PartitionedBucketScheme;
 import com.rapleaf.cascading_ext.tap.bucket2.ThriftBucketScheme;
+import com.rapleaf.cascading_ext.workflow2.ActionCallback;
 import com.rapleaf.cascading_ext.workflow2.CascadingAction2;
 import com.rapleaf.cascading_ext.workflow2.PartitionFactory;
-import com.rapleaf.cascading_ext.tap.TapFactory;
 
 public class MSJTapAction<K extends Comparable> extends CascadingAction2 {
 
@@ -77,6 +78,24 @@ public class MSJTapAction<K extends Comparable> extends CascadingAction2 {
                       SplitGenerator splitGen,
                       PartitionableDataStore output,
                       PartitionFactory structureFactory) {
+    this(checkpointToken, tmpRoot,
+        properties,
+        inputs,
+        function,
+        splitGen,
+        output,
+        structureFactory,
+        new ActionCallback.Default());
+  }
+
+  public MSJTapAction(String checkpointToken, String tmpRoot,
+                      Map<Object, Object> properties,
+                      final ExtractorsList<K> inputs,
+                      MSJFunction<K> function,
+                      SplitGenerator splitGen,
+                      PartitionableDataStore output,
+                      PartitionFactory structureFactory,
+                      ActionCallback callback) {
     super(checkpointToken, tmpRoot, properties);
 
     final List<StoreExtractor<K>> asList = inputs.get();
@@ -92,7 +111,7 @@ public class MSJTapAction<K extends Comparable> extends CascadingAction2 {
       public Tap createTap() throws IOException {
         return new MSJTap<K>(getConfs(asList), new MSJScheme<K>());
       }
-    });
+    }, callback);
 
     pipe = new Each(pipe,
         function
