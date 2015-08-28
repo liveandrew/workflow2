10e1afea1285752a01fecc718f954ef85af6c658
Ben Podgursky <bpodgursky@liveramp.com>
2015-08-27 19:37:48 -0700

    increase to 5 min

--- src/java/com/liveramp/workflow_monitor/alerts/execution/alerts/DiedUnclean.java
+++ src/java/com/liveramp/workflow_monitor/alerts/execution/alerts/DiedUnclean.java
@@ -10,16 +10,19 @@ import com.liveramp.workflow_monitor.alerts.execution.ExecutionAlert;
 import com.liveramp.workflow_monitor.alerts.execution.ExecutionAlertGenerator;
 import com.rapleaf.db_schemas.IDatabases;
 import com.rapleaf.db_schemas.rldb.models.WorkflowExecution;
+import com.rapleaf.db_schemas.rldb.workflow.DbPersistence;
 import com.rapleaf.db_schemas.rldb.workflow.WorkflowQueries;
 
 public class DiedUnclean implements ExecutionAlertGenerator {
+
+  private static final int MISSED_HEARTBEATS_THRESHOLD = DbPersistence.NUM_HEARTBEAT_TIMEOUTS * 5; // 5 min, to reduce false alarms
 
   @Override
   public List<ExecutionAlert> generateAlerts(IDatabases db) throws IOException {
 
     List<ExecutionAlert> alerts = Lists.newArrayList();
 
-    for (WorkflowExecution execution : WorkflowQueries.getDiedUncleanExecutions(db, null, 7)) {
+    for (WorkflowExecution execution : WorkflowQueries.getDiedUncleanExecutions(db, null, 7, MISSED_HEARTBEATS_THRESHOLD)) {
       long id = execution.getId();
       alerts.add(new ExecutionAlert(id,
           "Execution has died without shutting down cleanly.  This often means the process was killed by the system OOM killer.  Please cancel or resume the execution.",
