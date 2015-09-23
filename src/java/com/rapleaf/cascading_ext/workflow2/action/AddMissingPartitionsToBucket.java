87b0b250e54b6c077181795be89b7ac7ee9093c0
Soerian Lieve <slieve@liveramp.com>
2015-09-23 10:51:15 -0700

    Accept different # of partitions

--- src/java/com/rapleaf/cascading_ext/workflow2/action/AddMissingPartitionsToBucket.java
+++ src/java/com/rapleaf/cascading_ext/workflow2/action/AddMissingPartitionsToBucket.java
@@ -36,10 +36,11 @@ public class AddMissingPartitionsToBucket<T> extends Action {
   public static void main(String[] args) throws Exception {
     String path = args[0];
     Class clazz = Class.forName(args[1]);
+    int numPartitions = args.length > 2 ? Integer.parseInt(args[2]) : 599;
     BucketDataStore dataStore = new BucketDataStoreImpl(FileSystemHelper.getFS(), "bucket", path, "", clazz);
     new WorkflowRunner(
         AddMissingPartitionsToBucket.class,
-        new Step(new AddMissingPartitionsToBucket("checkpoint", 599, dataStore, clazz))
+        new Step(new AddMissingPartitionsToBucket("checkpoint", numPartitions, dataStore, clazz))
     ).run();
   }
 }
