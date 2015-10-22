7df1df98bcd9ea914b3027485531bcf14ef3ce78
Ben Podgursky <bpodgursky@liveramp.com>
2015-10-22 10:39:33 -0700

    ugh

--- src/java/com/rapleaf/cascading_ext/workflow2/WorkflowNotificationLevel.java
+++ src/java/com/rapleaf/cascading_ext/workflow2/WorkflowNotificationLevel.java
@@ -3,6 +3,8 @@ package com.rapleaf.cascading_ext.workflow2;
 import java.util.EnumSet;
 import java.util.Set;
 
+import com.google.common.collect.Sets;
+
 import com.rapleaf.db_schemas.rldb.workflow.WorkflowRunnerNotification;
 import com.rapleaf.support.collections.SetBuilder;
 
@@ -23,19 +25,19 @@ public class WorkflowNotificationLevel {
 
   public static final Set<WorkflowRunnerNotification> NONE = EnumSet.noneOf(WorkflowRunnerNotification.class);
 
-  public static final Set<WorkflowRunnerNotification> ERROR = new SetBuilder<>(NONE)
+  public static final Set<WorkflowRunnerNotification> ERROR = new SetBuilder<>(Sets.newHashSet(NONE))
       .add(WorkflowRunnerNotification.FAILURE, WorkflowRunnerNotification.DIED_UNCLEAN, WorkflowRunnerNotification.INTERNAL_ERROR)
       .get();
 
-  public static final Set<WorkflowRunnerNotification> WARN = new SetBuilder<>(ERROR)
+  public static final Set<WorkflowRunnerNotification> WARN = new SetBuilder<>(Sets.newHashSet(ERROR))
       .add(WorkflowRunnerNotification.SHUTDOWN)
       .get();
 
-  public static final Set<WorkflowRunnerNotification> INFO = new SetBuilder<>(WARN)
+  public static final Set<WorkflowRunnerNotification> INFO = new SetBuilder<>(Sets.newHashSet(WARN))
       .add(WorkflowRunnerNotification.SUCCESS)
       .get();
 
-  public static final Set<WorkflowRunnerNotification> DEBUG = new SetBuilder<>(INFO)
+  public static final Set<WorkflowRunnerNotification> DEBUG = new SetBuilder<>(Sets.newHashSet(INFO))
       .add(WorkflowRunnerNotification.START)
       .get();
 
