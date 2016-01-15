21fff5d97d41c2d11d06ff963aa0dbc79f797cb2
Jared Rhizor <jrhizor@liveramp.com>
2016-01-15 15:53:07 -0800

    Merge pull request #49 from MasterRepos/add_output_to_IdActionFactory_generic_types
    Make IdActionFactory take triple

--- src/java/com/rapleaf/cascading_ext/workflow2/util/IdActionFactory.java
+++ src/java/com/rapleaf/cascading_ext/workflow2/util/IdActionFactory.java
@@ -1,7 +1,9 @@
 package com.rapleaf.cascading_ext.workflow2.util;
 
+import org.apache.commons.lang3.tuple.Triple;
+
 import com.liveramp.java_support.functional.Fn;
 import com.rapleaf.cascading_ext.workflow2.Action;
 
-public interface IdActionFactory<Id> extends Fn<Id, Action> {
+public interface IdActionFactory<ID, OUTPUT> extends Fn<Triple<ID, String, OUTPUT>, Action> {
 }
