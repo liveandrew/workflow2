0d4107379796f2eebe7879934c973e96a7303227
Ben Podgursky <bpodgursky@liveramp.com>
2016-03-03 16:38:26 -0800

    Merge pull request #54 from MasterRepos/migrate_zk
    support multiple frameworks, lock in all provided frameworks, and look f...

--- src/java/com/liveramp/cascading_ext/megadesk/CuratorStoreReaderLockProvider.java
+++ src/java/com/liveramp/cascading_ext/megadesk/CuratorStoreReaderLockProvider.java
@@ -1,5 +1,8 @@
 package com.liveramp.cascading_ext.megadesk;
 
+import java.util.List;
+
+import com.google.common.collect.Lists;
 import org.apache.commons.codec.binary.Hex;
 import org.apache.curator.framework.CuratorFramework;
 
@@ -8,20 +11,28 @@ import com.rapleaf.support.HashHelper;
 
 public class CuratorStoreReaderLockProvider extends StoreReaderLockProvider {
 
-  public final CuratorFramework framework;
+  public final List<CuratorFramework> frameworks;
 
   public static CuratorStoreReaderLockProvider getProduction() {
-    return new CuratorStoreReaderLockProvider(ProductionCuratorProvider.INSTANCE.getFramework());
+    return new CuratorStoreReaderLockProvider(Lists.newArrayList(
+        //  TODO remove first one after everything deployed et all
+        ProductionCuratorProvider.INSTANCE.getFramework(),
+        ProductionCuratorProvider.INSTANCE.getFrameworkNew()
+    ));
   }
 
-  public CuratorStoreReaderLockProvider(CuratorFramework framework) {
-    this.framework = framework;
+  public CuratorStoreReaderLockProvider(CuratorFramework ... frameworks){
+    this(Lists.newArrayList(frameworks));
+  }
+
+  public CuratorStoreReaderLockProvider(List<CuratorFramework> frameworks) {
+    this.frameworks = frameworks;
   }
 
   @Override
   public ResourceSemaphore createLock(String path) {
     System.out.println("Semaphore on path "+path);
     String hashedPath = Hex.encodeHexString(HashHelper.getMD5Hash(path));
-    return new ResourceSemaphoreImpl(framework, hashedPath, path);
+    return new ResourceSemaphoreImpl(frameworks, hashedPath, path);
   }
 }
--- src/java/com/liveramp/cascading_ext/megadesk/ResourceSemaphoreImpl.java
+++ src/java/com/liveramp/cascading_ext/megadesk/ResourceSemaphoreImpl.java
@@ -2,24 +2,41 @@ package com.liveramp.cascading_ext.megadesk;
 
 import java.util.List;
 
+import com.google.common.collect.HashMultimap;
 import com.google.common.collect.Lists;
+import com.google.common.collect.Multimap;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.recipes.locks.InterProcessSemaphoreV2;
 import org.apache.curator.framework.recipes.locks.Lease;
 
 public class ResourceSemaphoreImpl implements ResourceSemaphore {
 
-  private final InterProcessSemaphoreV2 semaphore;
-  private final List<Lease> leases = Lists.newArrayList();
+
+  private final List<InterProcessSemaphoreV2> semaphores = Lists.newArrayList();
+  private final Multimap<InterProcessSemaphoreV2, Lease> leasesPerSemaphore = HashMultimap.create();
+
   public static final String READER_LOCK_BASE_PATH = "/clockwork/datastore_reader_locks/";
   private final String path;
   private final String name;
 
-  public ResourceSemaphoreImpl(CuratorFramework framework, String resourceUniqueIdentifer, String name) {
+  public ResourceSemaphoreImpl(CuratorFramework framework,
+                               String resourceUniqueIdentifer, String name) {
+    this(Lists.newArrayList(framework), resourceUniqueIdentifer, name);
+  }
+
+  public ResourceSemaphoreImpl(List<CuratorFramework> frameworks,
+                               String resourceUniqueIdentifer, String name) {
     this.name = name;
     this.path = READER_LOCK_BASE_PATH + resourceUniqueIdentifer;
-    this.semaphore = new InterProcessSemaphoreV2(framework, path, Integer.MAX_VALUE);
-    initNode(semaphore);
+
+    for (CuratorFramework framework : frameworks) {
+      this.semaphores.add(new InterProcessSemaphoreV2(framework, path, Integer.MAX_VALUE));
+    }
+
+    for (InterProcessSemaphoreV2 semaphore : semaphores) {
+      initNode(semaphore);
+    }
+
   }
 
   private void initNode(InterProcessSemaphoreV2 semaphore) {
@@ -36,8 +53,12 @@ public class ResourceSemaphoreImpl implements ResourceSemaphore {
   @Override
   public void lock() {
     try {
-      Lease lease = semaphore.acquire();
-      leases.add(lease);
+
+      for (InterProcessSemaphoreV2 semaphore : semaphores) {
+        Lease lease = semaphore.acquire();
+        leasesPerSemaphore.put(semaphore, lease);
+      }
+
     } catch (Exception e) {
       throw new RuntimeException(e);
     }
@@ -46,8 +67,12 @@ public class ResourceSemaphoreImpl implements ResourceSemaphore {
   @Override
   public void release() {
     try {
-      semaphore.returnAll(leases);
-      leases.clear();
+
+      for (InterProcessSemaphoreV2 semaphore : semaphores) {
+        semaphore.returnAll(leasesPerSemaphore.get(semaphore));
+        leasesPerSemaphore.removeAll(semaphore);
+      }
+
     } catch (Exception e) {
       throw new RuntimeException(e);
     }
@@ -56,7 +81,15 @@ public class ResourceSemaphoreImpl implements ResourceSemaphore {
   @Override
   public boolean hasReaders() {
     try {
-      return semaphore.getParticipantNodes().size() > 0;
+
+      for (InterProcessSemaphoreV2 semaphore : semaphores) {
+        if (semaphore.getParticipantNodes().size() > 0) {
+          return true;
+        }
+      }
+
+      return false;
+
     } catch (Exception e) {
       throw new RuntimeException(e);
     }
