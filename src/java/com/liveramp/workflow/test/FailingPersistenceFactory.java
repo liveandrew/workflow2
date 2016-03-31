5d599053baa0e81909f62d797d9695486b63a5a2
Soerian Lieve <slieve@liveramp.com>
2016-03-31 14:58:55 -0700

    Merge pull request #58 from MasterRepos/fail_intentionally
    Allow steps to fail intentionally in tests

---
+++ src/java/com/liveramp/workflow/test/FailingPersistenceFactory.java
@@ -0,0 +1,230 @@
+package com.liveramp.workflow.test;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import com.google.common.collect.Sets;
+import org.jgrapht.DirectedGraph;
+import org.jgrapht.graph.DefaultEdge;
+
+import com.liveramp.commons.collections.nested_map.ThreeNestedMap;
+import com.liveramp.commons.collections.nested_map.TwoNestedMap;
+import com.liveramp.importer.generated.AppType;
+import com.liveramp.java_support.alerts_handler.AlertsHandler;
+import com.liveramp.java_support.functional.Fn;
+import com.liveramp.java_support.functional.Fns;
+import com.liveramp.java_support.workflow.TaskSummary;
+import com.liveramp.workflow_state.AttemptStatus;
+import com.liveramp.workflow_state.StepState;
+import com.liveramp.workflow_state.StepStatus;
+import com.liveramp.workflow_state.WorkflowRunnerNotification;
+import com.liveramp.workflow_state.WorkflowStatePersistence;
+import com.rapleaf.cascading_ext.workflow2.Step;
+import com.rapleaf.cascading_ext.workflow2.state.WorkflowPersistenceFactory;
+
+public class FailingPersistenceFactory implements WorkflowPersistenceFactory {
+
+  private final WorkflowPersistenceFactory delegate;
+  private final Set<String> stepsToFailFullNames;
+
+
+  public FailingPersistenceFactory(WorkflowPersistenceFactory delegate, StepNameBuilder stepNameBuilder) {
+    this(delegate, Sets.newHashSet(stepNameBuilder));
+  }
+
+  /**
+   * @param stepNameBuilders any given steps are failed after they finish executing
+   */
+  public FailingPersistenceFactory(WorkflowPersistenceFactory delegate, Set<StepNameBuilder> stepNameBuilders) {
+    this.delegate = delegate;
+    this.stepsToFailFullNames = Sets.newHashSet(Fns.map(new Fn<StepNameBuilder, String>() {
+      @Override
+      public String apply(StepNameBuilder input) {
+        return input.getCompositeStepName();
+      }
+    }, stepNameBuilders));
+  }
+
+  @Override
+  public WorkflowStatePersistence prepare(DirectedGraph<Step, DefaultEdge> flatSteps,
+                                          String name,
+                                          String scopeId,
+                                          String description,
+                                          AppType appType,
+                                          String host,
+                                          String username,
+                                          String pool,
+                                          String priority,
+                                          String launchDir,
+                                          String launchJar,
+                                          Set<WorkflowRunnerNotification> configuredNotifications,
+                                          AlertsHandler configuredHandler,
+                                          String remote,
+                                          String implementationBuild) {
+    return new FailingPersistence(delegate.prepare(flatSteps, name, scopeId, description, appType, host, username, pool, priority, launchDir, launchJar, configuredNotifications, configuredHandler, remote, implementationBuild), stepsToFailFullNames);
+  }
+
+  public static class IntentionallyFailedStepException extends RuntimeException {
+    public IntentionallyFailedStepException(String message) {
+      super(message);
+    }
+  }
+
+  private static class FailingPersistence implements WorkflowStatePersistence {
+
+    private final WorkflowStatePersistence delegate;
+    private final Set<String> stepsToFailFullNames;
+
+    private FailingPersistence(WorkflowStatePersistence delegate, Set<String> stepsToFailFullNames) {
+      this.delegate = delegate;
+      this.stepsToFailFullNames = stepsToFailFullNames;
+    }
+
+    @Override
+    public void markStepStatusMessage(String stepToken, String newMessage) throws IOException {
+      delegate.markStepStatusMessage(stepToken, newMessage);
+    }
+
+    @Override
+    public void markStepRunningJob(String stepToken, String jobId, String jobName, String trackingURL) throws IOException {
+      delegate.markStepRunningJob(stepToken, jobId, jobName, trackingURL);
+    }
+
+    @Override
+    public void markJobCounters(String stepToken, String jobId, TwoNestedMap<String, String, Long> values) throws IOException {
+      delegate.markJobCounters(stepToken, jobId, values);
+    }
+
+    @Override
+    public void markJobTaskInfo(String stepToken, String jobId, TaskSummary info) throws IOException {
+      delegate.markJobTaskInfo(stepToken, jobId, info);
+    }
+
+    @Override
+    public void markStepRunning(String stepToken) throws IOException {
+      delegate.markStepRunning(stepToken);
+    }
+
+    @Override
+    public void markStepFailed(String stepToken, Throwable t) throws IOException {
+      delegate.markStepFailed(stepToken, t);
+    }
+
+    @Override
+    public void markStepCompleted(String stepToken) throws IOException {
+      delegate.markStepCompleted(stepToken);
+      if (stepsToFailFullNames.contains(stepToken)) {
+        IntentionallyFailedStepException exception = new IntentionallyFailedStepException(String.format("Failed step intentionally: %s", stepToken));
+        delegate.markStepFailed(stepToken, exception);
+        throw exception;
+      }
+    }
+
+    @Override
+    public void markWorkflowStarted() throws IOException {
+      delegate.markWorkflowStarted();
+    }
+
+    @Override
+    public void markWorkflowStopped() throws IOException {
+      delegate.markWorkflowStopped();
+    }
+
+    @Override
+    public void markStepReverted(String stepToken) throws IOException {
+      delegate.markStepReverted(stepToken);
+    }
+
+    @Override
+    public void markPool(String pool) throws IOException {
+      delegate.markPool(pool);
+    }
+
+    @Override
+    public void markPriority(String priority) throws IOException {
+      delegate.markPriority(priority);
+    }
+
+    @Override
+    public void markShutdownRequested(String reason) throws IOException {
+      delegate.markShutdownRequested(reason);
+    }
+
+    @Override
+    public StepStatus getStatus(String stepToken) throws IOException {
+      return delegate.getStatus(stepToken);
+    }
+
+    @Override
+    public Map<String, StepStatus> getStepStatuses() throws IOException {
+      return delegate.getStepStatuses();
+    }
+
+    @Override
+    public Map<String, StepState> getStepStates() throws IOException {
+      return delegate.getStepStates();
+    }
+
+    @Override
+    public String getShutdownRequest() throws IOException {
+      return delegate.getShutdownRequest();
+    }
+
+    @Override
+    public String getPriority() throws IOException {
+      return delegate.getPriority();
+    }
+
+    @Override
+    public String getPool() throws IOException {
+      return delegate.getPool();
+    }
+
+    @Override
+    public String getName() throws IOException {
+      return delegate.getName();
+    }
+
+    @Override
+    public String getScopeIdentifier() throws IOException {
+      return delegate.getScopeIdentifier();
+    }
+
+    @Override
+    public String getId() throws IOException {
+      return delegate.getId();
+    }
+
+    @Override
+    public AttemptStatus getStatus() throws IOException {
+      return delegate.getStatus();
+    }
+
+    @Override
+    public List<AlertsHandler> getRecipients(WorkflowRunnerNotification notification) throws IOException {
+      return delegate.getRecipients(notification);
+    }
+
+    @Override
+    public long getExecutionId() throws IOException {
+      return delegate.getExecutionId();
+    }
+
+    @Override
+    public long getAttemptId() throws IOException {
+      return delegate.getAttemptId();
+    }
+
+    @Override
+    public ThreeNestedMap<String, String, String, Long> getCountersByStep() throws IOException {
+      return delegate.getCountersByStep();
+    }
+
+    @Override
+    public TwoNestedMap<String, String, Long> getFlatCounters() throws IOException {
+      return delegate.getFlatCounters();
+    }
+  }
+}
---
+++ test/java/com/rapleaf/cascading_ext/workflow2/state/TestFailingPersistence.java
@@ -0,0 +1,124 @@
+package com.rapleaf.cascading_ext.workflow2.state;
+
+import java.io.IOException;
+import java.util.Set;
+
+import com.google.common.collect.Sets;
+import org.junit.Test;
+
+import com.liveramp.java_support.functional.Fn;
+import com.liveramp.java_support.functional.Fns;
+import com.rapleaf.cascading_ext.workflow2.Action;
+import com.rapleaf.cascading_ext.workflow2.MultiStepAction;
+import com.rapleaf.cascading_ext.workflow2.Step;
+import com.rapleaf.cascading_ext.workflow2.WorkflowRunner;
+import com.rapleaf.cascading_ext.workflow2.WorkflowTestCase;
+import com.rapleaf.cascading_ext.workflow2.options.TestWorkflowOptions;
+import com.liveramp.workflow.test.FailingPersistenceFactory;
+import com.liveramp.workflow.test.StepNameBuilder;
+
+import static org.junit.Assert.assertTrue;
+
+public class TestFailingPersistence extends WorkflowTestCase {
+
+  private static final String WORKFLOW_CHKPT_TOKEN = "test-workflow";
+  private static final String SHOULD_FAIL_CHKPT_TOKEN = "should-fail-but-complete";
+  private static final String NOT_REACHED_CHKPT_TOKEN = "should-not-be-reached";
+  private static final String SHOULDN_NOT_FAIL_MULTISTEP_CHKPT_TOKEN = "should-not-fail-multi-step";
+  private static final Set<StepNameBuilder> STEPS_THAT_SHOULD_COMPLETE = Sets.newHashSet(
+      new StepNameBuilder(WORKFLOW_CHKPT_TOKEN, SHOULDN_NOT_FAIL_MULTISTEP_CHKPT_TOKEN, SHOULD_FAIL_CHKPT_TOKEN),
+      new StepNameBuilder(WORKFLOW_CHKPT_TOKEN, SHOULD_FAIL_CHKPT_TOKEN)
+  );
+
+  @Test
+  public void testFailingPersistence() throws IOException {
+    Set<String> stepsThatCompleted = Sets.newHashSet();
+
+    boolean failedIntentionally = false;
+    try {
+      new WorkflowRunner(
+          TestWorkflow.class,
+          new FailingPersistenceFactory(new DbPersistenceFactory(), new StepNameBuilder(WORKFLOW_CHKPT_TOKEN, SHOULD_FAIL_CHKPT_TOKEN)),
+          new TestWorkflowOptions(),
+          new Step(new TestWorkflow(
+              WORKFLOW_CHKPT_TOKEN,
+              getTestRoot(),
+              stepsThatCompleted
+          ))
+      ).run();
+    } catch (RuntimeException e) {
+      failedIntentionally = e.getMessage().contains(FailingPersistenceFactory.IntentionallyFailedStepException.class.getName());
+    }
+    assertTrue(failedIntentionally);
+
+    assertCollectionEquivalent(Fns.map(new Fn<StepNameBuilder, String>() {
+      @Override
+      public String apply(StepNameBuilder input) {
+        return input.getCompositeStepName();
+      }
+    }, STEPS_THAT_SHOULD_COMPLETE), stepsThatCompleted);
+  }
+
+  private static class TestWorkflow extends MultiStepAction {
+
+    public TestWorkflow(String checkpointToken, String tmpRoot, Set<String> stepsThatSucceeded) {
+      super(checkpointToken, tmpRoot);
+
+      Step shouldNotFailMultiStep = new Step(new ShouldNotFailMultiStep(
+          checkpointToken,
+          SHOULDN_NOT_FAIL_MULTISTEP_CHKPT_TOKEN,
+          tmpRoot,
+          stepsThatSucceeded
+      ));
+
+      Step shouldFail = new Step(new TestStep(
+          checkpointToken,
+          SHOULD_FAIL_CHKPT_TOKEN,
+          stepsThatSucceeded
+      ),
+           shouldNotFailMultiStep);
+
+      Step shouldNotFail = new Step(new TestStep(
+          checkpointToken,
+          NOT_REACHED_CHKPT_TOKEN,
+          stepsThatSucceeded
+      ), shouldFail);
+
+      setSubStepsFromTail(shouldNotFail);
+    }
+  }
+
+  private static class ShouldNotFailMultiStep extends MultiStepAction {
+
+    public ShouldNotFailMultiStep(String superStepsCheckPointTokens, String checkpointToken, String tmpRoot, Set<String> stepsThatSucceeded) {
+      super(checkpointToken, tmpRoot);
+
+      Step shouldNotFailButHasSameNameAsStepThatFails = new Step(new TestStep(
+          new StepNameBuilder(superStepsCheckPointTokens, checkpointToken).getCompositeStepName(),
+          SHOULD_FAIL_CHKPT_TOKEN,
+          stepsThatSucceeded
+      ));
+
+      setSubStepsFromTail(shouldNotFailButHasSameNameAsStepThatFails);
+    }
+  }
+
+  private static class TestStep extends Action {
+
+    private final String superStepsCheckPointTokens;
+    private final String checkpointToken;
+    private final Set<String> stepsThatSucceeded;
+
+    public TestStep(String superStepsCheckPointTokens, String checkpointToken, Set<String> stepsThatSucceeded) {
+      super(checkpointToken);
+      this.superStepsCheckPointTokens = superStepsCheckPointTokens;
+      this.checkpointToken = checkpointToken;
+      this.stepsThatSucceeded = stepsThatSucceeded;
+    }
+
+    @Override
+    protected void execute() throws Exception {
+      stepsThatSucceeded.add(new StepNameBuilder(superStepsCheckPointTokens, checkpointToken).getCompositeStepName());
+    }
+  }
+}
