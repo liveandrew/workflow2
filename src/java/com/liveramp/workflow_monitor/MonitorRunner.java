71791ba677db48ac41fc36c7e91f7f2bedfa7203
Ben Podgursky <bpodgursky@liveramp.com>
2016-04-08 17:04:16 -0700

    remove second monitor runner

--- src/java/com/liveramp/workflow_monitor/MonitorRunner.java
+++ src/java/com/liveramp/workflow_monitor/MonitorRunner.java
@@ -2,9 +2,6 @@ package com.liveramp.workflow_monitor;
 
 import com.google.common.collect.Lists;
 
-import com.liveramp.java_support.alerts_handler.AlertsHandlers;
-import com.liveramp.java_support.alerts_handler.recipients.AlertRecipients;
-import com.liveramp.java_support.alerts_handler.recipients.TeamList;
 import com.liveramp.java_support.logging.LoggingHelper;
 import com.liveramp.workflow_monitor.alerts.execution.ExecutionAlertGenerator;
 import com.liveramp.workflow_monitor.alerts.execution.ExecutionAlerter;
@@ -16,7 +13,6 @@ import com.liveramp.workflow_monitor.alerts.execution.alerts.KilledTasks;
 import com.liveramp.workflow_monitor.alerts.execution.alerts.NearMemoryLimit;
 import com.liveramp.workflow_monitor.alerts.execution.alerts.OutputPerMapTask;
 import com.liveramp.workflow_monitor.alerts.execution.recipient.FromPersistenceGenerator;
-import com.liveramp.workflow_monitor.alerts.execution.recipient.TestRecipientGenerator;
 import com.rapleaf.db_schemas.DatabasesImpl;
 import com.rapleaf.db_schemas.IDatabases;
 
@@ -43,28 +39,27 @@ public class MonitorRunner {
         db
     );
 
-    ExecutionAlerter testing = new ExecutionAlerter(
-        new TestRecipientGenerator(
-            AlertsHandlers.builder(TeamList.DEV_TOOLS)
-                .setEngineeringRecipient(AlertRecipients.of("bpodgursky+alert-firehose@liveramp.com"))
-                .build()),
-        Lists.<ExecutionAlertGenerator>newArrayList(
-            new DiedUnclean()
-        ),
-        Lists.<MapreduceJobAlertGenerator>newArrayList(
-            new KilledTasks(),
-            new GCTime(),
-            new NearMemoryLimit(),
-            new CPUUsage(),
-            new OutputPerMapTask()
-        ),
-        db
-    );
+//    ExecutionAlerter testing = new ExecutionAlerter(
+//        new TestRecipientGenerator(
+//            AlertsHandlers.builder(TeamList.DEV_TOOLS)
+//                .setEngineeringRecipient(AlertRecipients.of("bpodgursky+alert-firehose@liveramp.com"))
+//                .build()),
+//        Lists.<ExecutionAlertGenerator>newArrayList(
+//            new DiedUnclean()
+//        ),
+//        Lists.<MapreduceJobAlertGenerator>newArrayList(
+//            new KilledTasks(),
+//            new GCTime(),
+//            new NearMemoryLimit(),
+//            new CPUUsage(),
+//            new OutputPerMapTask()
+//        ),
+//        db
+//    );
 
     WorkflowMonitor monitor = new WorkflowMonitor(
         Lists.newArrayList(
-            production,
-            testing
+            production
         )
     );
 
