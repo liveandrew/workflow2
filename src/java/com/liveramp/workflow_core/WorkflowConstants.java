efd992ba34388387345764bd398c57c3c691f2a2
Ben Podgursky <bpodgursky@liveramp.com>
2016-07-27 09:51:57 -0700

    move class here

---
+++ src/java/com/liveramp/workflow_core/WorkflowConstants.java
@@ -0,0 +1,9 @@
+package com.liveramp.workflow_core;
+
+public class WorkflowConstants {
+  /**
+   * Specify this and the system will pick any free port.j
+   */
+  public static final String WORKFLOW_EMAIL_SUBJECT_TAG = "WORKFLOW";
+  public static final String ERROR_EMAIL_SUBJECT_TAG = "ERROR";
+}
