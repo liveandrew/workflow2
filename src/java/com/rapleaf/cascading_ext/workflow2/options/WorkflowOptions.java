7deeda778467c2e2d05f6b5d441b804e4f8158c6
Ben Podgursky <bpodgursky@liveramp.com>
2016-07-26 17:19:05 -0700

    move stuff upstream

--- src/java/com/rapleaf/cascading_ext/workflow2/options/WorkflowOptions.java
+++ src/java/com/rapleaf/cascading_ext/workflow2/options/WorkflowOptions.java
@@ -1,12 +1,8 @@
 package com.rapleaf.cascading_ext.workflow2.options;
 
-import java.util.EnumSet;
 import java.util.Map;
-import java.util.Set;
 
-import com.google.common.base.Optional;
 import com.google.common.collect.Maps;
-import com.google.common.collect.Sets;
 
 import com.liveramp.cascading_ext.megadesk.StoreReaderLockProvider;
 import com.liveramp.cascading_ext.resource.ResourceDeclarer;
@@ -14,37 +10,29 @@ import com.liveramp.cascading_ext.util.HadoopProperties;
 import com.liveramp.cascading_tools.properties.PropertiesUtil;
 import com.liveramp.commons.collections.properties.NestedProperties;
 import com.liveramp.commons.collections.properties.OverridableProperties;
-import com.liveramp.importer.generated.AppType;
-import com.liveramp.java_support.alerts_handler.AlertsHandler;
-import com.liveramp.java_support.alerts_handler.AlertsHandlers;
 import com.liveramp.java_support.alerts_handler.recipients.TeamList;
-import com.liveramp.workflow_state.WorkflowRunnerNotification;
+import com.liveramp.workflow_core.BaseWorkflowOptions;
 import com.rapleaf.cascading_ext.CascadingHelper;
 import com.rapleaf.cascading_ext.workflow2.ContextStorage;
 import com.rapleaf.cascading_ext.workflow2.TrackerURLBuilder;
 import com.rapleaf.cascading_ext.workflow2.counter.CounterFilter;
 
-public class WorkflowOptions {
+public class WorkflowOptions extends BaseWorkflowOptions<WorkflowOptions> {
 
-  private int maxConcurrentSteps;
-  private AlertsHandler alertsHandler;
-  private OverridableProperties properties = new NestedProperties(Maps.newHashMap(), false);
-  private Set<WorkflowRunnerNotification> enabledNotifications;
-  private Set<WorkflowRunnerNotification> disabledNotifications = Sets.newHashSet();
   private StoreReaderLockProvider lockProvider;
   private ContextStorage storage;
-  private String uniqueIdentifier;
-  private AppType appType;
-  private Integer stepPollInterval;
   private CounterFilter counterFilter;
   private ResourceDeclarer resourceDeclarer;
-  private String sandboxDir;
+
   private TrackerURLBuilder urlBuilder;
   private HostnameProvider hostnameProvider;
-  private String description;
+
+  private OverridableProperties properties = new NestedProperties(Maps.newHashMap(), false);
+
 
   protected WorkflowOptions() {
   }
+
 
   public HostnameProvider getHostnameProvider() {
     return hostnameProvider;
@@ -55,14 +43,6 @@ public class WorkflowOptions {
     return this;
   }
 
-  public String getDescription() {
-    return description;
-  }
-
-  public WorkflowOptions setDescription(String description) {
-    this.description = description;
-    return this;
-  }
 
   public WorkflowOptions setUrlBuilder(TrackerURLBuilder urlBuilder) {
     this.urlBuilder = urlBuilder;
@@ -73,72 +53,6 @@ public class WorkflowOptions {
     return urlBuilder;
   }
 
-  public int getMaxConcurrentSteps() {
-    return maxConcurrentSteps;
-  }
-
-  public WorkflowOptions setMaxConcurrentSteps(int maxConcurrentSteps) {
-    this.maxConcurrentSteps = maxConcurrentSteps;
-    return this;
-  }
-
-  public WorkflowOptions setStepPollInterval(int ms) {
-    this.stepPollInterval = ms;
-    return this;
-  }
-
-  public Integer getStepPollInterval() {
-    return stepPollInterval;
-  }
-
-  public AlertsHandler getAlertsHandler() {
-    return alertsHandler;
-  }
-
-  public WorkflowOptions configureTeam(TeamList team, String subPool) {
-    addWorkflowProperties(PropertiesUtil.teamPool(team, subPool));
-    setAlertsHandler(team);
-    return this;
-  }
-
-  public WorkflowOptions setAlertsHandler(TeamList team) {
-    return setAlertsHandler(team, Optional.<Class<?>>absent());
-  }
-
-  public WorkflowOptions setAlertsHandler(TeamList team, Class<?> project) {
-    return setAlertsHandler(team, Optional.<Class<?>>of(project));
-  }
-
-  public WorkflowOptions setAlertsHandler(TeamList team, Optional<Class<?>> project) {
-    return setAlertsHandler(AlertsHandlers.buildHandlerForTeam(team, project));
-  }
-
-  public WorkflowOptions setAlertsHandler(AlertsHandler alertsHandler) {
-    this.alertsHandler = alertsHandler;
-    return this;
-  }
-
-  public WorkflowOptions setEnabledNotifications(WorkflowRunnerNotification enabledNotification,
-                                                 WorkflowRunnerNotification... enabledNotifications) {
-    this.enabledNotifications = EnumSet.of(enabledNotification, enabledNotifications);
-    return this;
-  }
-
-  public WorkflowOptions setEnabledNotifications(Set<WorkflowRunnerNotification> enabledNotifications) {
-    this.enabledNotifications = EnumSet.copyOf(enabledNotifications);
-    return this;
-  }
-
-  public WorkflowOptions setDisabledNotifications(WorkflowRunnerNotification disabledNotification,
-                                                  WorkflowRunnerNotification... disabledNotifications) {
-    this.disabledNotifications = EnumSet.of(disabledNotification, disabledNotifications);
-    return this;
-  }
-
-  public WorkflowOptions setNotificationLevel(Set<WorkflowRunnerNotification> notifications) {
-    enabledNotifications = EnumSet.copyOf(notifications);
-    return this;
-  }
 
   public WorkflowOptions addWorkflowProperties(Map<Object, Object> propertiesMap) {
     return addWorkflowHadoopProperties(new HadoopProperties(propertiesMap, false));
@@ -149,6 +63,25 @@ public class WorkflowOptions {
     return this;
   }
 
+  public WorkflowOptions configureTeam(TeamList team, String subPool) {
+    addWorkflowProperties(PropertiesUtil.teamPool(team, subPool));
+    setAlertsHandler(team);
+    return this;
+  }
+
+
+  public CounterFilter getCounterFilter() {
+    return counterFilter;
+  }
+
+  @Deprecated
+  //  all counters are tracked by default now, so this will actually disable tracking most counters.  if you really want this, let me know  -- ben
+  public WorkflowOptions setCounterFilter(CounterFilter filter) {
+    this.counterFilter = filter;
+    return this;
+  }
+
+
   public WorkflowOptions setResourceManager(ResourceDeclarer resourceManager) {
     this.resourceDeclarer = resourceManager;
     return this;
@@ -158,9 +91,6 @@ public class WorkflowOptions {
     return this.resourceDeclarer;
   }
 
-  public Set<WorkflowRunnerNotification> getEnabledNotifications() {
-    return Sets.difference(enabledNotifications, disabledNotifications);
-  }
 
   public OverridableProperties getWorkflowJobProperties() {
     return properties.override(CascadingHelper.get().getDefaultHadoopProperties());
@@ -184,45 +114,5 @@ public class WorkflowOptions {
     return this;
   }
 
-  public String getScopeIdentifier() {
-    return uniqueIdentifier;
-  }
-
-  public WorkflowOptions setUniqueIdentifier(String uniqueIdentifier) {
-    if (uniqueIdentifier != null && uniqueIdentifier.equals("__NULL")) {
-      throw new IllegalArgumentException("This is temporarily a reserved scope, while making scope not null");
-    }
-    this.uniqueIdentifier = uniqueIdentifier;
-    return this;
-  }
-
-  public AppType getAppType() {
-    return appType;
-  }
-
-  public WorkflowOptions setAppType(AppType appType) {
-    this.appType = appType;
-    return this;
-  }
-
-  public CounterFilter getCounterFilter() {
-    return counterFilter;
-  }
-
-  @Deprecated
-  //  all counters are tracked by default now, so this will actually disable tracking most counters.  if you really want this, let me know  -- ben
-  public WorkflowOptions setCounterFilter(CounterFilter filter) {
-    this.counterFilter = filter;
-    return this;
-  }
-
-  public String getSandboxDir() {
-    return sandboxDir;
-  }
-
-  public WorkflowOptions setSandboxDir(String sandboxDir) {
-    this.sandboxDir = sandboxDir;
-    return this;
-  }
 
 }
