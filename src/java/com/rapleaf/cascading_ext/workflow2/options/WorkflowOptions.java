63ee82c6b836050f3ab795fb86dc609e4f08c3dd
Ben Podgursky <bpodgursky@liveramp.com>
2016-07-28 13:19:46 -0700

    use configured props

--- src/java/com/rapleaf/cascading_ext/workflow2/options/WorkflowOptions.java
+++ src/java/com/rapleaf/cascading_ext/workflow2/options/WorkflowOptions.java
@@ -3,11 +3,10 @@ package com.rapleaf.cascading_ext.workflow2.options;
 import java.util.Map;
 
 import com.google.common.collect.Maps;
+import org.apache.hadoop.mapred.JobConf;
 
 import com.liveramp.cascading_ext.megadesk.StoreReaderLockProvider;
 import com.liveramp.cascading_tools.properties.PropertiesUtil;
-import com.liveramp.commons.collections.properties.NestedProperties;
-import com.liveramp.commons.collections.properties.OverridableProperties;
 import com.liveramp.java_support.alerts_handler.recipients.TeamList;
 import com.liveramp.workflow_core.BaseWorkflowOptions;
 import com.rapleaf.cascading_ext.CascadingHelper;
@@ -20,19 +19,16 @@ public class WorkflowOptions extends BaseWorkflowOptions<WorkflowOptions> {
   private ContextStorage storage;
   private CounterFilter counterFilter;
 
-  private OverridableProperties properties = new NestedProperties(Maps.newHashMap(), false);
-
-
   protected WorkflowOptions() {
+    super(CascadingHelper.get().getDefaultHadoopProperties(),  toProperties(CascadingHelper.get().getJobConf()));
   }
 
-  public WorkflowOptions addWorkflowProperties(Map<Object, Object> propertiesMap) {
-    return addWorkflowHadoopProperties(new NestedProperties(propertiesMap, false));
-  }
-
-  public WorkflowOptions addWorkflowHadoopProperties(OverridableProperties newProperties) {
-    this.properties = newProperties.override(this.properties);
-    return this;
+  private static Map<Object, Object> toProperties(JobConf conf){
+    Map<Object, Object> props = Maps.newHashMap();
+    for (Map.Entry<String, String> entry : conf) {
+      props.put(entry.getKey(), entry.getValue());
+    }
+    return props;
   }
 
   public WorkflowOptions configureTeam(TeamList team, String subPool) {
@@ -40,7 +36,6 @@ public class WorkflowOptions extends BaseWorkflowOptions<WorkflowOptions> {
     setAlertsHandler(team);
     return this;
   }
-
 
   public CounterFilter getCounterFilter() {
     return counterFilter;
@@ -51,11 +46,6 @@ public class WorkflowOptions extends BaseWorkflowOptions<WorkflowOptions> {
   public WorkflowOptions setCounterFilter(CounterFilter filter) {
     this.counterFilter = filter;
     return this;
-  }
-
-
-  public OverridableProperties getWorkflowJobProperties() {
-    return properties.override(CascadingHelper.get().getDefaultHadoopProperties());
   }
 
   public ContextStorage getStorage() {
@@ -75,6 +65,5 @@ public class WorkflowOptions extends BaseWorkflowOptions<WorkflowOptions> {
     this.lockProvider = lockProvider;
     return this;
   }
-
 
 }
