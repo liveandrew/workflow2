a38533b34463d02230d989ed3e0c73d228e75c7c
Jared Rhizor <jrhizor@liveramp.com>
2016-08-04 14:27:10 -0700

    simplify IdActionFactory

--- src/java/com/rapleaf/cascading_ext/workflow2/util/IdActionFactory.java
+++ src/java/com/rapleaf/cascading_ext/workflow2/util/IdActionFactory.java
@@ -1,9 +1,7 @@
 package com.rapleaf.cascading_ext.workflow2.util;
 
-import org.apache.commons.lang3.tuple.Triple;
-
 import com.liveramp.java_support.functional.Fn;
 import com.liveramp.workflow_core.runner.BaseAction;
 
-public interface IdActionFactory<ID, OUTPUT> extends Fn<Triple<ID, String, OUTPUT>, BaseAction> {
+public interface IdActionFactory<CONTEXT> extends Fn<CONTEXT, BaseAction> {
 }
