11e7fdc898734c6cad183e485ef0e814c6974cc4
Ben Podgursky <bpodgursky@liveramp.com>
2016-08-22 14:14:33 -0700

    helper

--- src/java/com/liveramp/workflow_state/runner/WorkflowRunners.java
+++ src/java/com/liveramp/workflow_state/runner/WorkflowRunners.java
@@ -1,6 +1,7 @@
 package com.liveramp.workflow_state.runner;
 
 import java.io.IOException;
+import java.util.Set;
 
 import com.google.common.collect.Sets;
 
@@ -8,6 +9,7 @@ import com.liveramp.workflow_core.BaseWorkflowOptions;
 import com.liveramp.workflow_core.runner.BaseStep;
 import com.rapleaf.cascading_ext.workflow2.BaseWorkflowRunner;
 import com.rapleaf.cascading_ext.workflow2.state.BaseDbPersistenceFactory;
+import com.rapleaf.cascading_ext.workflow2.state.InitializedWorkflow;
 
 public class WorkflowRunners {
 
@@ -17,6 +19,10 @@ public class WorkflowRunners {
 
   public static BaseWorkflowRunner<Void> baseRlDbWorkflowRunner(BaseWorkflowOptions options, BaseStep step) throws IOException {
     return new BaseWorkflowRunner(new BaseDbPersistenceFactory().initialize(options), Sets.newHashSet(step), null);
+  }
+
+  public static BaseWorkflowRunner<Void> baseRldbWorkflowRunner(InitializedWorkflow workflow, Set<BaseStep<Void>> steps) throws IOException {
+    return new BaseWorkflowRunner<>(workflow, steps, null);
   }
 
 }
