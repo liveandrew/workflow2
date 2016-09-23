e859c53c8864dc10f88e17f212cb2875eb99c3ab
Lane Erickson <lerickson@liveramp.com>
2016-09-23 12:27:06 -0700

    this will have to slowly be renamed over time

--- src/java/com/liveramp/workflow_db_state/controller/ApplicationController.java
+++ src/java/com/liveramp/workflow_db_state/controller/ApplicationController.java
@@ -33,24 +33,24 @@ import com.liveramp.workflow_state.WorkflowRunnerNotification;
 //  TODO not liking all the staticness of this.  figure out later
 public class ApplicationController {
 
-  public static void revertStep(IWorkflowDb rldb, AppType app, String scopeIdentifier, String stepToken) throws IOException {
+  public static void revertStep(IWorkflowDb db, AppType app, String scopeIdentifier, String stepToken) throws IOException {
 
-    Optional<WorkflowExecution> execution = WorkflowQueries.getLatestExecution(rldb, app, scopeIdentifier);
+    Optional<WorkflowExecution> execution = WorkflowQueries.getLatestExecution(db, app, scopeIdentifier);
 
     if(execution.isPresent()){
 
       WorkflowAttempt attempt = WorkflowQueries.getLatestAttempt(execution.get());
 
-      DbPersistence attemptController = DbPersistence.queryPersistence(attempt.getId(), rldb);
+      DbPersistence attemptController = DbPersistence.queryPersistence(attempt.getId(), db);
       attemptController.markStepReverted(stepToken);
 
     }
 
   }
 
-  public static Map<String, StepState> getLatestStepStates(IWorkflowDb rldb, AppType app, String scopeIdentifier) throws IOException {
+  public static Map<String, StepState> getLatestStepStates(IWorkflowDb db, AppType app, String scopeIdentifier) throws IOException {
 
-    Optional<WorkflowExecution> latestExecution = WorkflowQueries.getLatestExecution(rldb, app, scopeIdentifier);
+    Optional<WorkflowExecution> latestExecution = WorkflowQueries.getLatestExecution(db, app, scopeIdentifier);
     Map<String, StepState> latestStatus = Maps.newHashMap();
 
     if(latestExecution.isPresent()){
@@ -59,7 +59,7 @@ public class ApplicationController {
       Collections.sort(attempts);
 
       for (WorkflowAttempt attempt : attempts) {
-        for (Map.Entry<String, StepState> entry : DbPersistence.queryPersistence(attempt.getId(), rldb).getStepStates().entrySet()) {
+        for (Map.Entry<String, StepState> entry : DbPersistence.queryPersistence(attempt.getId(), db).getStepStates().entrySet()) {
           StepState value = entry.getValue();
           if(!value.getStatus().equals(StepStatus.SKIPPED)){
             latestStatus.put(entry.getKey(), value);
@@ -75,22 +75,22 @@ public class ApplicationController {
     ExecutionController.cancelExecution(db, WorkflowQueries.getLatestExecution(db, workflowName, scopeIdentifier));
   }
 
-  public static void cancelLatestExecution(IWorkflowDb rlDb, AppType appType, String scopeIdentifier) throws IOException {
-    Optional<WorkflowExecution> latestExecution = WorkflowQueries.getLatestExecution(rlDb, appType, scopeIdentifier);
+  public static void cancelLatestExecution(IWorkflowDb db, AppType appType, String scopeIdentifier) throws IOException {
+    Optional<WorkflowExecution> latestExecution = WorkflowQueries.getLatestExecution(db, appType, scopeIdentifier);
     if (latestExecution.isPresent()) {
-      ExecutionController.cancelExecution(rlDb, latestExecution.get());
+      ExecutionController.cancelExecution(db, latestExecution.get());
     }
   }
 
-  public static void cancelIfIncompleteExecution(IWorkflowDb rlDb, AppType appType, String scopeIdentifier) throws IOException {
-    Optional<WorkflowExecution> latestExecution = WorkflowQueries.getLatestExecution(rlDb, appType, scopeIdentifier);
+  public static void cancelIfIncompleteExecution(IWorkflowDb db, AppType appType, String scopeIdentifier) throws IOException {
+    Optional<WorkflowExecution> latestExecution = WorkflowQueries.getLatestExecution(db, appType, scopeIdentifier);
     if (isIncomplete(latestExecution)) {
-      ExecutionController.cancelExecution(rlDb, latestExecution.get());
+      ExecutionController.cancelExecution(db, latestExecution.get());
     }
   }
 
-  public static boolean isRunning(IWorkflowDb rlDb, AppType appType, String scopeIdentifier) throws IOException {
-    Optional<WorkflowExecution> latestExecution = WorkflowQueries.getLatestExecution(rlDb, appType, scopeIdentifier);
+  public static boolean isRunning(IWorkflowDb db, AppType appType, String scopeIdentifier) throws IOException {
+    Optional<WorkflowExecution> latestExecution = WorkflowQueries.getLatestExecution(db, appType, scopeIdentifier);
     if (latestExecution.isPresent()) {
       return ExecutionController.isRunning(latestExecution.get());
     } else {
@@ -98,8 +98,8 @@ public class ApplicationController {
     }
   }
 
-  public static boolean isLatestExecutionIncomplete(IWorkflowDb rlDb, AppType appType, String scopeIdentifier) throws IOException {
-    return isIncomplete(WorkflowQueries.getLatestExecution(rlDb, appType, scopeIdentifier));
+  public static boolean isLatestExecutionIncomplete(IWorkflowDb db, AppType appType, String scopeIdentifier) throws IOException {
+    return isIncomplete(WorkflowQueries.getLatestExecution(db, appType, scopeIdentifier));
   }
 
   public static int numRunningInstances(IDatabases db, AppType appType) throws IOException {
@@ -116,40 +116,40 @@ public class ApplicationController {
   }
 
 
-  public static void addConfiguredNotifications(IWorkflowDb rlDb, String workflowName, String email, Set<WorkflowRunnerNotification> notifications) throws IOException {
-    Application application = Accessors.only(rlDb.applications().findByName(workflowName));
+  public static void addConfiguredNotifications(IWorkflowDb db, String workflowName, String email, Set<WorkflowRunnerNotification> notifications) throws IOException {
+    Application application = Accessors.only(db.applications().findByName(workflowName));
 
     Set<WorkflowRunnerNotification> existing = Sets.newHashSet();
-    for (ConfiguredNotification.Attributes attributes : WorkflowQueries.getApplicationNotifications(rlDb, application.getId(), email)) {
+    for (ConfiguredNotification.Attributes attributes : WorkflowQueries.getApplicationNotifications(db, application.getId(), email)) {
       existing.add(WorkflowRunnerNotification.findByValue(attributes.getWorkflowRunnerNotification()));
     }
 
     for (WorkflowRunnerNotification notification : notifications) {
       if (!existing.contains(notification)) {
-        ConfiguredNotification configured = rlDb.configuredNotifications().create(notification.ordinal(), email, false);
-        rlDb.applicationConfiguredNotifications().create(application.getId(), configured.getId());
+        ConfiguredNotification configured = db.configuredNotifications().create(notification.ordinal(), email, false);
+        db.applicationConfiguredNotifications().create(application.getId(), configured.getId());
       }
     }
 
   }
 
-  public static void removeConfiguredNotifications(IWorkflowDb rlDb, String workflowName, String email) throws IOException {
-    removeConfiguredNotifications(rlDb, workflowName, email, EnumSet.allOf(WorkflowRunnerNotification.class));
+  public static void removeConfiguredNotifications(IWorkflowDb db, String workflowName, String email) throws IOException {
+    removeConfiguredNotifications(db, workflowName, email, EnumSet.allOf(WorkflowRunnerNotification.class));
   }
 
-  public static void removeConfiguredNotifications(IWorkflowDb rlDb, String workflowName, String email, Set<WorkflowRunnerNotification> notificaions) throws IOException {
-    Application application = Accessors.only(rlDb.applications().findByName(workflowName));
+  public static void removeConfiguredNotifications(IWorkflowDb db, String workflowName, String email, Set<WorkflowRunnerNotification> notificaions) throws IOException {
+    Application application = Accessors.only(db.applications().findByName(workflowName));
     long appId = application.getId();
 
-    for (ConfiguredNotification.Attributes attributes : WorkflowQueries.getApplicationNotifications(rlDb, appId, email)) {
+    for (ConfiguredNotification.Attributes attributes : WorkflowQueries.getApplicationNotifications(db, appId, email)) {
       if (notificaions.contains(WorkflowRunnerNotification.findByValue(attributes.getWorkflowRunnerNotification()))) {
 
-        for (ApplicationConfiguredNotification appNotification : rlDb.applicationConfiguredNotifications().query()
+        for (ApplicationConfiguredNotification appNotification : db.applicationConfiguredNotifications().query()
             .applicationId(appId)
             .configuredNotificationId(attributes.getId())
             .find()) {
 
-          rlDb.applicationConfiguredNotifications().delete(appNotification);
+          db.applicationConfiguredNotifications().delete(appNotification);
           //  TODO if a ConfiguredNotification has no App/Execution/Attempt ConfiguredNotifications referencing it, delete it
         }
       }
