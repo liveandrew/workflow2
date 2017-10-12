a78964dcd853ebaf5ac217348512f86d1ad30975
Ben Podgursky <bpodgursky@liveramp.com>
2017-10-12 14:58:28 -0700

    Merge pull request #16 from MasterRepos/check_type
    Add config type to NoOp step

--- src/java/com/liveramp/workflow_core/step/NoOp.java
+++ src/java/com/liveramp/workflow_core/step/NoOp.java
@@ -2,7 +2,7 @@ package com.liveramp.workflow_core.step;
 
 import com.liveramp.workflow_core.runner.BaseAction;
 
-public class NoOp extends BaseAction {
+public class NoOp<Config> extends BaseAction<Config> {
 
   public NoOp(String checkpointToken) {
     super(checkpointToken);
