95412efd9c9a317cc4cf5d4dfdb503431a09f0d8
Ben Podgursky <bpodgursky@liveramp.com>
2017-11-17 16:00:17 -0800

    add data

--- src/java/com/liveramp/workflow_state/ExecutionState.java
+++ src/java/com/liveramp/workflow_state/ExecutionState.java
@@ -5,11 +5,13 @@ public class ExecutionState {
   private final Long startTime;
   private final Long endTime;
   private final String appName;
+  private final int numAttempts;
 
-  public ExecutionState(Long startTime, Long endTime, String appName) {
+  public ExecutionState(Long startTime, Long endTime, String appName, int numAttempts) {
     this.startTime = startTime;
     this.endTime = endTime;
     this.appName = appName;
+    this.numAttempts = numAttempts;
   }
 
   public Long getStartTime() {
@@ -23,4 +25,10 @@ public class ExecutionState {
   public String getAppName() {
     return appName;
   }
+
+
+  public int getNumAttempts() {
+    return numAttempts;
+  }
+
 }
