9c6f3c5314bc4701734a994908f3244ef5d846e9
Porter Westling <pwestling@liveramp.com>
2018-02-16 16:17:59 -0800

    Merge pull request #98 from MasterRepos/max_wait_protections
    protect against jobs not getting resources at the right priority, or from hanging forever

--- src/java/com/liveramp/workflow/backpressure/RMJMXFlowSubmissionController.java
+++ src/java/com/liveramp/workflow/backpressure/RMJMXFlowSubmissionController.java
@@ -26,43 +26,72 @@ public class RMJMXFlowSubmissionController implements FlowSubmissionController {
   private final TimeUnit sleepUnit;
   private final long sleepAmount;
   private IOFunction<String, String> jsonRetriever;
+  private final TimeUnit maximumWaitUnit;
+  private final long maximumWaitAmount;
+  private final long maxWaitDurationMillis;
 
   public static RMJMXFlowSubmissionController production() {
-    return new RMJMXFlowSubmissionController(5000, TimeUnit.MINUTES, 1, LRHttpUtils::GETRequest);
+    return production(TimeUnit.HOURS, 6);
+  }
+
+  public static RMJMXFlowSubmissionController production(TimeUnit maxWaitUnit, long maxWaitAmount) {
+    return new RMJMXFlowSubmissionController(
+        15000,
+        TimeUnit.MINUTES,
+        1,
+        maxWaitUnit,
+        maxWaitAmount,
+        LRHttpUtils::GETRequest);
   }
 
 
-  RMJMXFlowSubmissionController(long pendingContainerLimit, TimeUnit sleepUnit, long sleepAmount, IOFunction<String, String> jsonRetriever) {
+  RMJMXFlowSubmissionController(
+      long pendingContainerLimit,
+      TimeUnit sleepUnit, long sleepAmount,
+      TimeUnit maximumWaitUnit, long maximumWaitAmount,
+      IOFunction<String, String> jsonRetriever) {
     this.pendingContainerLimit = pendingContainerLimit;
     this.sleepUnit = sleepUnit;
     this.sleepAmount = sleepAmount;
+    this.maximumWaitAmount = maximumWaitAmount;
+    this.maximumWaitUnit = maximumWaitUnit;
     this.jsonRetriever = jsonRetriever;
+    this.maxWaitDurationMillis = maximumWaitUnit.toMillis(maximumWaitAmount);
+
   }
 
   @Override
   public void blockUntilSubmissionAllowed(Configuration flowConfig) {
     try {
       String mapReduceQueue = flowConfig.get(MRJobConfig.QUEUE_NAME);
+      long waitStart = System.currentTimeMillis();
+      long maxWaitTimestamp = waitStart + maxWaitDurationMillis;
       long pendingContainersForQueue = getPendingContainersForQueue(mapReduceQueue);
-      while (pendingContainersForQueue > pendingContainerLimit) {
-        long sleepMillis = determineSleepMilliseconds(pendingContainersForQueue);
+      while (pendingContainersForQueue > pendingContainerLimit && System.currentTimeMillis() < maxWaitTimestamp) {
+        long sleepMillis = determineSleepMilliseconds(pendingContainersForQueue, maxWaitTimestamp - System.currentTimeMillis());
         LOG.info("There are " + pendingContainersForQueue + " pending containers in the queue compared to the limit of " + pendingContainerLimit
             + ". Delaying job submission for " + sleepUnit.convert(sleepMillis, TimeUnit.MILLISECONDS) + " " + sleepUnit.name());
         TimeUnit.MILLISECONDS.sleep(sleepMillis);
         pendingContainersForQueue = getPendingContainersForQueue(mapReduceQueue);
+      }
+      if ((System.currentTimeMillis() - waitStart) >= maxWaitDurationMillis) {
+        LOG.warn("Waited for more than the max wait period of " + maximumWaitAmount + " " + maximumWaitUnit.name() + ". Launching job.");
       }
     } catch (InterruptedException e) {
       LOG.error("Error while blocking for job submission. Allowing job to launch", e);
     }
   }
 
-  long determineSleepMilliseconds(long pendingContainersForQueue) {
+  long determineSleepMilliseconds(long pendingContainersForQueue, long maxSleepMillis) {
     //If there's a large backlog, we should wait longer before checking again if it's clear.
     // Use a log base 2 to ensure we never wait for a really massive time
     double pendingRatio = pendingContainersForQueue / (double)pendingContainerLimit;
     double logOfRatio = Math.log(pendingRatio) / Math.log(2); //log base 2
     double sleepMultiplier = Math.ceil(logOfRatio);
-    return (long)(sleepUnit.toMillis(sleepAmount) * sleepMultiplier);
+    long computedSleepMilliseconds = (long)(sleepUnit.toMillis(sleepAmount) * sleepMultiplier);
+
+    //We also compute how many milliseconds until our max wait runs out, and use that if it's smaller
+    return Math.min(computedSleepMilliseconds, Math.max(maxSleepMillis, 0));
   }
 
   private long getPendingContainersForQueue(String mapReduceQueue) {
--- test/java/com/liveramp/workflow/backpressure/TestRMJMXFlowSubmissionController.java
+++ test/java/com/liveramp/workflow/backpressure/TestRMJMXFlowSubmissionController.java
@@ -11,8 +11,6 @@ import org.apache.log4j.Level;
 import org.junit.Assert;
 import org.junit.Test;
 
-import com.liveramp.cascading_tools.util.RMJMXPendingContainerChecker;
-import com.liveramp.java_support.functional.Fn;
 import com.liveramp.java_support.functional.IOFunction;
 import com.rapleaf.java_support.CommonJUnit4TestCase;
 
@@ -31,7 +29,10 @@ public class TestRMJMXFlowSubmissionController extends CommonJUnit4TestCase {
     IOFunction<String, String> jsonRetriever = queue -> "{\"beans\":[{\"PendingContainers\":" + itr.next() + "}]}";
 
     RMJMXFlowSubmissionController controller =
-        new RMJMXFlowSubmissionController(9, TimeUnit.SECONDS, 1, jsonRetriever);
+        new RMJMXFlowSubmissionController(9,
+            TimeUnit.MILLISECONDS, 100,
+            TimeUnit.HOURS, 1,
+            jsonRetriever);
 
     Configuration conf = new Configuration();
     conf.set(MRJobConfig.QUEUE_NAME, "root.team.queue");
@@ -39,8 +40,8 @@ public class TestRMJMXFlowSubmissionController extends CommonJUnit4TestCase {
     long start = System.currentTimeMillis();
     controller.blockUntilSubmissionAllowed(conf);
     long passed = System.currentTimeMillis() - start;
-    Assert.assertTrue(passed > 1000); //we slept for 1 second if this went correctly
-    Assert.assertFalse(passed > 2000); //we didn't sleep twice
+    Assert.assertTrue(passed >= 100); //we slept for 1 wait period if this went correctly
+    Assert.assertTrue(passed < 200); //we didn't sleep twice
     Assert.assertFalse(itr.hasNext()); //we checked the pending containers twice and exhausted our dummy sequence
   }
 
@@ -49,7 +50,11 @@ public class TestRMJMXFlowSubmissionController extends CommonJUnit4TestCase {
     IOFunction<String, String> jsonRetriever = queue -> "{\"beans\":[{\"PendingContainers\":5}]}";
 
     RMJMXFlowSubmissionController controller =
-        new RMJMXFlowSubmissionController(9, TimeUnit.SECONDS, 1, jsonRetriever);
+        new RMJMXFlowSubmissionController(
+            9,
+            TimeUnit.SECONDS, 1,
+            TimeUnit.HOURS, 1,
+            jsonRetriever);
 
     Configuration conf = new Configuration();
     conf.set(MRJobConfig.QUEUE_NAME, "root.team.queue");
@@ -61,23 +66,73 @@ public class TestRMJMXFlowSubmissionController extends CommonJUnit4TestCase {
   }
 
   @Test
+  public void testMaxWait() {
+    //Setup mocks so that there are always too many containers, so we should hit max wait
+    IOFunction<String, String> jsonRetriever = queue -> "{\"beans\":[{\"PendingContainers\":5}]}";
+
+    RMJMXFlowSubmissionController controller =
+        new RMJMXFlowSubmissionController(
+            4,
+            TimeUnit.MILLISECONDS, 10,
+            TimeUnit.MILLISECONDS, 100,
+            jsonRetriever);
+
+    Configuration conf = new Configuration();
+    conf.set(MRJobConfig.QUEUE_NAME, "root.team.queue");
+
+    long start = System.currentTimeMillis();
+    controller.blockUntilSubmissionAllowed(conf);
+    long passed = System.currentTimeMillis() - start;
+    Assert.assertTrue(passed < 120 && passed >= 100);
+  }
+
+  @Test
+  public void testMaxWaitPremptsWait() {
+    //Setup mocks so that there are always too many containers, so we should hit max wait
+    IOFunction<String, String> jsonRetriever = queue -> "{\"beans\":[{\"PendingContainers\":5}]}";
+
+    RMJMXFlowSubmissionController controller =
+        new RMJMXFlowSubmissionController(
+            4,
+            TimeUnit.SECONDS, 1,
+            TimeUnit.MILLISECONDS, 100,
+            jsonRetriever);
+
+    Configuration conf = new Configuration();
+    conf.set(MRJobConfig.QUEUE_NAME, "root.team.queue");
+
+    long start = System.currentTimeMillis();
+    controller.blockUntilSubmissionAllowed(conf);
+    long passed = System.currentTimeMillis() - start;
+    Assert.assertTrue(passed < 120 && passed >= 100);
+  }
+
+  @Test
   public void determineSleepMilliseconds() throws Exception {
     RMJMXFlowSubmissionController controller =
-        new RMJMXFlowSubmissionController(5000, TimeUnit.MINUTES, 1, s -> s);
-    Assert.assertEquals(TimeUnit.MINUTES.toMillis(0), controller.determineSleepMilliseconds(3000));
-    Assert.assertEquals(TimeUnit.MINUTES.toMillis(1), controller.determineSleepMilliseconds(5001));
-    Assert.assertEquals(TimeUnit.MINUTES.toMillis(2), controller.determineSleepMilliseconds(10001));
-    Assert.assertEquals(TimeUnit.MINUTES.toMillis(2), controller.determineSleepMilliseconds(15000));
-    Assert.assertEquals(TimeUnit.MINUTES.toMillis(3), controller.determineSleepMilliseconds(20001));
-    Assert.assertEquals(TimeUnit.MINUTES.toMillis(4), controller.determineSleepMilliseconds(40001));
-    Assert.assertEquals(TimeUnit.MINUTES.toMillis(4), controller.determineSleepMilliseconds(40001));
-    Assert.assertEquals(TimeUnit.MINUTES.toMillis(7), controller.determineSleepMilliseconds(400000));
+        new RMJMXFlowSubmissionController(5000, TimeUnit.MINUTES, 1, TimeUnit.HOURS, 1, s -> s);
+    Assert.assertEquals(TimeUnit.MINUTES.toMillis(0), controller.determineSleepMilliseconds(3000, Integer.MAX_VALUE));
+    Assert.assertEquals(TimeUnit.MINUTES.toMillis(1), controller.determineSleepMilliseconds(5001, Integer.MAX_VALUE));
+    Assert.assertEquals(TimeUnit.MINUTES.toMillis(2), controller.determineSleepMilliseconds(10001, Integer.MAX_VALUE));
+    Assert.assertEquals(TimeUnit.MINUTES.toMillis(2), controller.determineSleepMilliseconds(15000, Integer.MAX_VALUE));
+    Assert.assertEquals(TimeUnit.MINUTES.toMillis(3), controller.determineSleepMilliseconds(20001, Integer.MAX_VALUE));
+    Assert.assertEquals(TimeUnit.MINUTES.toMillis(4), controller.determineSleepMilliseconds(40001, Integer.MAX_VALUE));
+    Assert.assertEquals(TimeUnit.MINUTES.toMillis(4), controller.determineSleepMilliseconds(40001, Integer.MAX_VALUE));
+    Assert.assertEquals(TimeUnit.MINUTES.toMillis(7), controller.determineSleepMilliseconds(400000, Integer.MAX_VALUE));
+
+    //Can't be larger than max wait
+    Assert.assertEquals(6000, controller.determineSleepMilliseconds(Integer.MAX_VALUE, 6000));
+
+    //Can't be negative
+    Assert.assertEquals(0, controller.determineSleepMilliseconds(Integer.MAX_VALUE, -6000));
+
+
   }
 
   @Test
   public void getContainersFromJson() throws Exception {
     RMJMXFlowSubmissionController controller =
-        new RMJMXFlowSubmissionController(5000, TimeUnit.MINUTES, 1, s -> s);
+        new RMJMXFlowSubmissionController(5000, TimeUnit.MINUTES, 1, TimeUnit.HOURS, 1, s -> s);
     Assert.assertEquals(10,
         controller.getContainersFromJson("some.queue", "{\"beans\":[{\"PendingContainers\":10}]}"));
     //if we get nothing back, react defensively and return 0 to let jobs run
