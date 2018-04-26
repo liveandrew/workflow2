9efecc8a7b4357f288668ba0ea0f541fcbfdef68
Ben Podgursky <bpodgursky@liveramp.com>
2018-04-25 18:56:20 -0700

    log

--- src/java/com/liveramp/cascading_ext/action/CopyToHdfs.java
+++ src/java/com/liveramp/cascading_ext/action/CopyToHdfs.java
@@ -1,12 +1,18 @@
 package com.liveramp.cascading_ext.action;
 
+import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.FsStatus;
 import org.apache.hadoop.fs.Path;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.liveramp.cascading_ext.FileSystemHelper;
 import com.rapleaf.cascading_ext.workflow2.Action;
 
 public class CopyToHdfs extends Action {
+  private static final Logger LOG = LoggerFactory.getLogger(CopyToHdfs.class);
+
   private final String inputPath;
   private final Path hdfsPath;
 
@@ -20,7 +26,16 @@ public class CopyToHdfs extends Action {
   protected void execute() throws Exception {
     setStatusMessage(String.format("Attempting to copy from %s to %s", inputPath, hdfsPath));
     FileSystem fs = FileSystemHelper.getFileSystemForPath(hdfsPath);
+
     fs.copyFromLocalFile(new Path(inputPath), hdfsPath);
+
+    FileStatus copiedStatus = fs.getFileStatus(hdfsPath);
+    if(copiedStatus == null){
+      throw new RuntimeException("File does not exist on HDFS!");
+    }
+
+    LOG.info("File copied to HDFS: "+copiedStatus.toString());
+
     setStatusMessage(String.format("Copied from %s to %s", inputPath, hdfsPath));
   }
 }
