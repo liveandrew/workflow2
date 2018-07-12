3b2312333cb1f6e0d5e0459034c280155e9c014a
Ben Podgursky <bpodgursky@liveramp.com>
2018-07-12 00:15:24 -0700

    fix

--- src/java/com/liveramp/workflow/test/FailingPersistenceFactory.java
+++ src/java/com/liveramp/workflow/test/FailingPersistenceFactory.java
@@ -14,6 +14,8 @@ import com.liveramp.importer.generated.AppType;
 import com.liveramp.java_support.alerts_handler.AlertsHandler;
 import com.liveramp.java_support.functional.Fn;
 import com.liveramp.java_support.functional.Fns;
+import com.liveramp.workflow.state.DbHadoopWorkflow;
+import com.liveramp.workflow_db_state.InitializedDbPersistence;
 import com.liveramp.workflow_state.IStep;
 import com.liveramp.workflow_state.InitializedPersistence;
 import com.liveramp.workflow_state.WorkflowRunnerNotification;
@@ -22,9 +24,9 @@ import com.rapleaf.cascading_ext.workflow2.options.WorkflowOptions;
 import com.rapleaf.cascading_ext.workflow2.state.InitializedWorkflow;
 import com.rapleaf.cascading_ext.workflow2.state.WorkflowPersistenceFactory;
 
-public class FailingPersistenceFactory<INITIALIZED extends InitializedPersistence, WORKFLOW extends InitializedWorkflow<INITIALIZED, WorkflowOptions>> extends WorkflowPersistenceFactory<INITIALIZED, WorkflowOptions, WORKFLOW> {
+public class FailingPersistenceFactory extends WorkflowPersistenceFactory<InitializedDbPersistence, WorkflowOptions, DbHadoopWorkflow> {
 
-  protected final WorkflowPersistenceFactory<INITIALIZED, WorkflowOptions, WORKFLOW> delegate;
+  protected final WorkflowPersistenceFactory<InitializedDbPersistence, WorkflowOptions, DbHadoopWorkflow> delegate;
   private final Set<String> stepsToFailFullNames;
 
   public FailingPersistenceFactory(WorkflowPersistenceFactory delegate, StepNameBuilder stepNameBuilder) {
@@ -45,17 +47,17 @@ public class FailingPersistenceFactory<INITIALIZED extends InitializedPersistenc
   }
 
   @Override
-  public <S extends IStep> WorkflowStatePersistence prepare(INITIALIZED persistence, DirectedGraph<S, DefaultEdge> flatSteps) {
+  public <S extends IStep> WorkflowStatePersistence prepare(InitializedDbPersistence persistence, DirectedGraph<S, DefaultEdge> flatSteps) {
     return new FailingPersistence(delegate.prepare(persistence, flatSteps), stepsToFailFullNames);
   }
 
   @Override
-  public WORKFLOW construct(String workflowName, WorkflowOptions options, INITIALIZED initialized, ResourceManager manager, MultiShutdownHook hook) {
-    return delegate.construct(workflowName, options, initialized, manager, hook);
+  public DbHadoopWorkflow construct(String workflowName, WorkflowOptions options, InitializedDbPersistence initialized, ResourceManager manager, MultiShutdownHook hook) {
+    return new DbHadoopWorkflow(workflowName, options, initialized, this, manager, hook);
   }
 
   @Override
-  public INITIALIZED initializeInternal(String name, String scopeId, String description, AppType appType, String host, String username, String pool, String priority, String launchDir, String launchJar, Set<WorkflowRunnerNotification> configuredNotifications, AlertsHandler providedHandler, Class<? extends ResourceDeclarerFactory> resourceFactory, String remote, String implementationBuild) throws IOException {
+  public InitializedDbPersistence initializeInternal(String name, String scopeId, String description, AppType appType, String host, String username, String pool, String priority, String launchDir, String launchJar, Set<WorkflowRunnerNotification> configuredNotifications, AlertsHandler providedHandler, Class<? extends ResourceDeclarerFactory> resourceFactory, String remote, String implementationBuild) throws IOException {
     return delegate.initializeInternal(name, scopeId, description, appType, host, username, pool, priority, launchDir, launchJar, configuredNotifications, providedHandler, resourceFactory, remote, implementationBuild);
   }
 
