824a98b0e22d802157b686154be823e82eb32850
Liren Tu <ltu@liveramp.com>
2018-09-07 01:26:24 -0700

    docs: add warning for persist-new-version action

--- src/java/com/rapleaf/cascading_ext/workflow2/action/PersistNewVersion.java
+++ src/java/com/rapleaf/cascading_ext/workflow2/action/PersistNewVersion.java
@@ -11,6 +11,13 @@ public class PersistNewVersion<T> extends Action {
   private final BucketDataStore versionToPersist;
   private final VersionedBucketDataStore store;
 
+  /**
+   * Move {@param versionToPersist} to {@param store} as its new version.
+   * <p>
+   * Caution: make sure {@param versionToPersist} is no longer needed in any action downstream of this one.
+   * Otherwise any dependent action will read from an empty store. About one hour has been spent on debugging
+   * issues from this gotcha.
+   */
   public PersistNewVersion(String checkpointToken, BucketDataStore<T> versionToPersist, VersionedBucketDataStore<T> store) {
     super(checkpointToken);
 
