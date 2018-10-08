6e26fe7aa6f318c26aabce8539b895a5f5505c2d
Ben Podgursky <bpodgursky@liveramp.com>
2018-10-08 11:42:51 -0700

    update entrypoint

--- container/entrypoint.sh
+++ container/entrypoint.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
-# TODO db migrate
+set -e
+
+cd /apps/workflow_db/
+
+bundle exec rake rapleaf:migrate
+
+cd /apps/workflow_ui/
 
 java -Djava.io.tmpdir=/var/www/tmp -Xmx12000m -Djava.net.preferIPv4Stack=true -cp workflow_ui.job.jar \
   com.liveramp.workflow_ui.WorkflowDbWebServer
-
