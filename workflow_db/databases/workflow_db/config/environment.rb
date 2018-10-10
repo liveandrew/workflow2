4920b9a1de0f6e1bc2f91aa56187bd550898878f
Ben Podgursky <bpodgursky@liveramp.com>
2018-10-10 09:30:01 -0700

    try this

--- workflow_db/databases/workflow_db/config/environment.rb
+++ workflow_db/databases/workflow_db/config/environment.rb
@@ -4,6 +4,9 @@ require File.expand_path('../application', __FILE__)
 gem 'activerecord'
 
 require 'thrift'
+require "rapleaf_types/enums_constants"
+require "rapleaf_types/new_person_data_constants"
+
 require 'rap_support'
 require 'rap_support/core_ext/active_record_extension' # Support for 'enum' and 'enum_from_thrift'
 
