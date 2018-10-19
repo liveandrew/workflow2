798fe56ec055e0bf5973ce06143fbae574231724
Ben Podgursky <bpodgursky@liveramp.com>
2018-10-18 17:13:58 -0700

    fix

--- workflow_ui/src/test/java/com/liveramp/workflow_ui/WorkflowUITestCase.java
+++ workflow_ui/src/test/java/com/liveramp/workflow_ui/WorkflowUITestCase.java
@@ -1,18 +1,11 @@
 package com.liveramp.workflow_ui;
 
-import org.apache.log4j.Level;
 import org.junit.Before;
-
-import com.rapleaf.java_support.CommonJUnit4TestCase;
 
 /**
  * Unit test for simple App.
  */
-public abstract class WorkflowUITestCase extends CommonJUnit4TestCase {
-
-  public WorkflowUITestCase() {
-    super(Level.ALL);
-  }
+public abstract class WorkflowUITestCase {
 
   @Before
   public void setUp() throws Exception {
