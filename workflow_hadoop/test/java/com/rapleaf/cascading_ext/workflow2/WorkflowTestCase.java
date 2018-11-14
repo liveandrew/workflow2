a6dd4e7591ec2f23e796850846c00e242238a912
Ben Podgursky <bpodgursky@liveramp.com>
2018-11-14 09:51:40 -0800

    cleanup

--- workflow_hadoop/test/java/com/rapleaf/cascading_ext/workflow2/WorkflowTestCase.java
+++ workflow_hadoop/test/java/com/rapleaf/cascading_ext/workflow2/WorkflowTestCase.java
@@ -1,8 +1,10 @@
 package com.rapleaf.cascading_ext.workflow2;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.concurrent.Callable;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
@@ -11,6 +13,7 @@ import org.junit.Before;
 
 import cascading.tuple.Fields;
 
+import com.liveramp.cascading_ext.fs.TrashHelper;
 import com.liveramp.databases.workflow_db.DatabasesImpl;
 
 import com.rapleaf.cascading_ext.datastore.TupleDataStore;
@@ -30,6 +33,12 @@ public class WorkflowTestCase  {
 
   public FileSystem getFS() throws IOException {
     return new Path(TEST_ROOT).getFileSystem(new Configuration());
+  }
+
+  @Before
+  public final void setUpTestRoot() throws IOException {
+    FileUtils.deleteDirectory(new File(this.TEST_ROOT));
+    TrashHelper.deleteUsingTrashIfEnabled(new Path(TEST_ROOT).getFileSystem(new Configuration()), new Path(this.TEST_ROOT));
   }
 
   @Before
