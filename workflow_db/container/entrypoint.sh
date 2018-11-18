f028f6356113ecb54a8b6a3b1571a77874ffcf40
Ben Podgursky <bpodgursky@liveramp.com>
2018-11-18 15:37:37 -0800

    fiddle with entrypoint

--- workflow_db/container/entrypoint.sh
+++ workflow_db/container/entrypoint.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 
-# set up the rails db
-
-cd /apps/workflow_db/
-
+# set up the database files from environment vars
 sed -e "s/\${db_username}/$DB_USERNAME/" \
   -e "s/\${db_password}/$DB_PASSWORD/" \
   -e "s/\${db_address}/$DB_HOSTNAME/" \
@@ -11,7 +8,20 @@ sed -e "s/\${db_username}/$DB_USERNAME/" \
   /apps/workflow_db/config/database.yml.tpl \
   > /apps/workflow_db/config/database.yml
 
-export RAILS_ENV=docker_env
+function mysql_conn_error {
+  mysql -h $DB_HOSTNAME -P $DB_PORT -u $DB_USERNAME --password=$DB_PASSWORD -e "show databases"
+  return $?
+}
 
+# wait until mysql is running
+while
+    mysql_conn_error
+do
+    sleep 1
+done
+
+# set up the rails db
+export RAILS_ENV=docker_env
+cd /apps/workflow_db/
 bundle exec rake db:create
 bundle exec rake rapleaf:migrate
