aba7ea39830783571c3d2e179a996daef0898d1a
Ben Podgursky <bpodgursky@liveramp.com>
2018-12-01 23:24:36 -0800

    fix

--- workflow_state/docker/start-db.sh
+++ workflow_state/docker/start-db.sh
@@ -9,4 +9,4 @@ docker-compose -f "$DIR/test-db-compose.yml" pull
 docker-compose -f "$DIR/test-db-compose.yml" up -d
 
 docker pull gcr.io/liveramp-eng/workflow2_db:latest
-docker run --rm --name test_migrate --network=docker_shared -e "DB_PORT=3306" -e "DB_USERNAME=root" -e "DB_HOSTNAME=mysql" gcr.io/liveramp-eng/workflow2_db:latest
+docker run --rm --name test_migrate --network=workflow_state_shared -e "DB_PORT=3306" -e "DB_USERNAME=root" -e "DB_HOSTNAME=mysql" gcr.io/liveramp-eng/workflow2_db:latest
