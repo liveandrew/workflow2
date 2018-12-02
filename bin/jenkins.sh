68e17f5c6683336a1cec9d15bba857a6047b6892
Ben Podgursky <bpodgursky@liveramp.com>
2018-12-01 20:08:18 -0800

    switch everything to eng

--- bin/jenkins.sh
+++ bin/jenkins.sh
@@ -7,20 +7,20 @@ docker pull centos:centos7
 # Workflow db migration container
 pushd workflow_db
 docker build -t workflow2_db:latest -f Dockerfile .
-docker tag workflow2_db gcr.io/liveramp-eng-bdi/workflow2_db
-docker push gcr.io/liveramp-eng-bdi/workflow2_db
+docker tag workflow2_db gcr.io/liveramp-eng/workflow2_db
+docker push gcr.io/liveramp-eng/workflow2_db
 popd
 
 # Workflow UI container
 pushd workflow_ui
 docker build -t workflow2_ui:latest -f Dockerfile .
-docker tag workflow2_ui gcr.io/liveramp-eng-bdi/workflow2_ui
-docker push gcr.io/liveramp-eng-bdi/workflow2_ui
+docker tag workflow2_ui gcr.io/liveramp-eng/workflow2_ui
+docker push gcr.io/liveramp-eng/workflow2_ui
 popd
 
 # Workflow monitor container
 pushd workflow_monitor
 docker build -t workflow2_monitor:latest -f Dockerfile .
-docker tag workflow2_monitor gcr.io/liveramp-eng-bdi/workflow2_monitor
-docker push gcr.io/liveramp-eng-bdi/workflow2_monitor
+docker tag workflow2_monitor gcr.io/liveramp-eng/workflow2_monitor
+docker push gcr.io/liveramp-eng/workflow2_monitor
 popd
