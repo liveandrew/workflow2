686443db459d4290a4f8debfabaa2d95370d2bdf
Ben Podgursky <bpodgursky@gmail.com>
2019-06-14 13:44:51 -0700

    Try to use local image when building on travis, so migrations are picked up in the same test build

--- test_docker/start-db.sh
+++ test_docker/start-db.sh
@@ -5,5 +5,5 @@ DIR="${0%/*}"
 
 $DIR/start-mysql.sh
 
-docker pull liveramp/workflow2_sqldump:latest
-docker run --rm --name test_migrate --network=workflow_test_shared -e "DB_PORT=3306" -e "DB_USERNAME=root" -e "DB_HOSTNAME=mysql" liveramp/workflow2_sqldump:latest
+docker pull ${SQLDUMP_REPO}
+docker run --rm --name test_migrate --network=workflow_test_shared -e "DB_PORT=3306" -e "DB_USERNAME=root" -e "DB_HOSTNAME=mysql" ${SQLDUMP_REPO}
--- workflow_db/bin/build-containers.sh
+++ workflow_db/bin/build-containers.sh
@@ -5,7 +5,7 @@ set -e
 source ${BASH_SOURCE%/*}/shared.sh
 
 docker build -t db_migrations:latest -f container/migration/Dockerfile .
-docker tag db_migrations ${MIGRATION_REPO}
+docker tag db_migrations:latest ${MIGRATION_REPO}
 
 docker build -t sqldump:latest -f container/sqldump/Dockerfile .
-docker tag sqldump ${SQLDUMP_REPO}
+docker tag sqldump:latest ${SQLDUMP_REPO}
