a1233609766f9f22facc37f0f4982a1c4e829495
Ben Podgursky <bpodgursky@gmail.com>
2019-06-25 17:24:56 -0700

    Add a more complex workflow example

---
+++ workflow_examples/src/main/java/com/liveramp/workflow2/workflow_examples/MoreComplexWorkflow.java
@@ -0,0 +1,46 @@
+package com.liveramp.workflow2.workflow_examples;
+
+import java.io.IOException;
+
+import com.google.common.collect.Sets;
+
+import com.liveramp.workflow2.workflow_examples.actions.FailingAction;
+import com.liveramp.workflow2.workflow_examples.actions.WaitAction;
+import com.rapleaf.cascading_ext.workflow2.Step;
+import com.rapleaf.cascading_ext.workflow2.WorkflowRunners;
+import com.rapleaf.cascading_ext.workflow2.action.NoOpAction;
+import com.rapleaf.cascading_ext.workflow2.options.HadoopWorkflowOptions;
+
+public class MoreComplexWorkflow {
+
+  public static void main(String[] args) throws IOException {
+
+    Step step1 = new Step(new NoOpAction("step1"));
+
+    Step step2 = new Step(new NoOpAction("step2"));
+
+    Step step3 = new Step(new NoOpAction("step3"));
+
+    Step step4 = new Step(new NoOpAction("step4"));
+
+    Step step5 = new Step(new WaitAction("step5", 180_000), step1, step2, step3, step4);
+
+    Step step6 = new Step(new FailingAction("step6", true), step1);
+
+    Step step7 = new Step(new NoOpAction("step7"), step5, step6);
+
+    Step step8 = new Step(new NoOpAction("step7"), step7);
+
+    Step step9 = new Step(new NoOpAction("step7"), step7);
+
+    WorkflowRunners.dbRun(
+        com.liveramp.workflow2.workflow_examples.SimpleWorkflow.class.getName(),
+        HadoopWorkflowOptions.test(),
+        dbHadoopWorkflow -> Sets.newHashSet(step8, step9)
+    );
+
+  }
+
+}
+
+}
